d87428f17cb2f7f6d19865d57eaac3afad1acf7d
Artem Oschepkov <artem.oschepkov@fastdev.se>
2014-01-01 04:26:24 -0800

    [UPD] Problem Module

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Models/Problem/Output/LogEditModel.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Models/Problem/Output/LogEditModel.cs
@@ -8,6 +8,8 @@
 
         public int ChangedByUserId { get; set; }
 
+        [Required]
+        [StringLength(2000)]
         public string LogText { get; set; }
 
         public int ShowOnCase { get; set; }
