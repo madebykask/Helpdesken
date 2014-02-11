15093eaadb6c1eaf815906ac492c84de829fc023
dh_majid_ghanadi <majid.ghanadi@dhsolutions.se>
2014-02-11 16:28:19 +0100

    [ADD] Questionnire primary file

---
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Controllers/QuestionnireController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DH.Helpdesk.Web.Models.Questionnire;
+namespace DH.Helpdesk.Web.Controllers
+{
+    public class QuestionnireController : Controller
+    {
+        //
+        // GET: /Questionnire/
+
+        public ActionResult Index()
+        {
+            return View();
+            
+        }
+
+    }
+}
