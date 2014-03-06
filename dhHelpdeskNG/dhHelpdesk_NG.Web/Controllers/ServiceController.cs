1c41f0029706afa9a13a66b4893ffd0a5e521b2c
Rustam Singatov <rustam.singatov@fastdev.se>
2014-03-06 17:59:00 +0400

    [UPD] Changes module.

---
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Controllers/ServiceController.cs
@@ -0,0 +1,22 @@
+﻿namespace DH.Helpdesk.Web.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    using DH.Helpdesk.Services.Services;
+    using DH.Helpdesk.Web.Infrastructure;
+
+    public sealed class ServiceController : BaseController
+    {
+        public ServiceController(IMasterDataService masterDataService)
+            : base(masterDataService)
+        {
+        }
+
+        [HttpPost]
+        public void RememberTab(string topic, string tab)
+        {
+            throw new Exception("Remember tab called.");
+        }
+    }
+}
--- dhHelpdeskNG/dhHelpdesk_NG.Web/Scripts/Changes/index.js
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Scripts/Changes/index.js
@@ -1,15 +1,29 @@
-﻿$(function() {
+﻿function applyBehavior(parameters) {
+    $('#case_summary_tab').click(function() {
+        $.post(parameters.rememberTabUrl, { topic: parameters.changeTopicName, tab: parameters.caseSummaryTabName });
+        $('#new_change_button').hide();
+        $('#save_settings_button').hide();
+    });
+
     $('#changes_tab').click(function() {
+        $.post(parameters.RememberTabUrl, { topic: parameters.changeTopicName, tab: parameters.changeTabName });
         $('#save_settings_button').hide();
         $('#new_change_button').show();
     });
 
+    $('#orders_tab').click(function() {
+        $.post(parameters.RememberTabUrl, { topic: parameters.changeTopicName, tab: parameters.ordersTabName });
+        $('#new_change_button').hide();
+        $('#save_settings_button').hide();
+    });
+
     $('#settings_tab').click(function() {
+        $.post(parameters.RememberTabUrl, { topic: parameters.changeTopicName, tab: parameters.settingsTabName });
         $('#new_change_button').hide();
         $('#save_settings_button').show();
     });
 
-    $('#save_settings_button').click(function () {
+    $('#save_settings_button').click(function() {
         $('#settings_form').submit();
     });
-});
+}
