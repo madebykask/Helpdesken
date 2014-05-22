4f9527761684c71bfa7c7d749271bc488917ab7d
Aleksei Matveev <alexey.matveev@fastdev.se>
2014-05-22 11:59:23 +0200

    [UPD] Downloading files.

---
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Infrastructure/Tools/MimeHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace DH.Helpdesk.Web.Infrastructure.Tools
+{
+    using System.IO;
+
+    using Microsoft.Win32;
+
+    public static class MimeHelper
+    {
+        private const string DefaultMimeType = "application/octet-stream";
+
+        public static string GetMimeType(string fileName)
+        {
+            var extension = Path.GetExtension(fileName);
+            if (string.IsNullOrEmpty(extension))
+            {
+                return DefaultMimeType;
+            }
+
+            var registryKey = Registry.ClassesRoot.OpenSubKey(extension);
+            if (registryKey == null)
+            {
+                return DefaultMimeType;
+            }
+
+            var mimeType = registryKey.GetValue("Content Type") as string;
+            if (string.IsNullOrEmpty(mimeType))
+            {
+                return DefaultMimeType;
+            }
+
+            return mimeType;
+        }
+    }
+}
