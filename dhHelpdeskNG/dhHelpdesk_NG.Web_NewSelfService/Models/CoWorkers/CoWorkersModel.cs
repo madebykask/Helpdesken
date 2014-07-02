c4dca07a8a44bb294bf3c4640104f9f21c86df37
dh_majid_ghanadi <majid.ghanadi@dhsolutions.se>
2014-07-02 16:25:21 +0200

    [ADD] CoWorkers Model to LM

---
+++ dhHelpdeskNG/dhHelpdesk_NG.Web_NewSelfService/Models/CoWorkers/CoWorkersModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace DH.Helpdesk.NewSelfService.Models.CoWorkers
+{
+    
+    using DH.Helpdesk.Domain;        
+    
+
+    public class CoWorkersModel 
+    {
+        public CoWorkersModel()
+        {             
+        }
+
+        public CoWorkersModel(List<CoWorkerTab> cwTabs, int activeTabId)
+        {
+            this.CoWorkersTabs = cwTabs;
+            this.ActiveTabId = activeTabId;
+        }
+
+        public List<CoWorkerTab> CoWorkersTabs { get; set; }
+
+        public int ActiveTabId { get; set; }
+                
+    }
+
+    public class CaseInfo
+    {
+        public int CaseNo { get; set; }
+
+        public string CaseName { get; set; }
+        
+    }
+
+    public class CoWorkerTab
+    {
+        public int Id { get; set; }
+
+        public List<CaseInfo> Cases { get; set; }
+    }
+
+
+}
