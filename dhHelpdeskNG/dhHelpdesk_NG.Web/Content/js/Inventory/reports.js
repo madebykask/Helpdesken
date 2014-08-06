4a916de5f6c8cfc08cbbb90d4c3a304a7dcc2648
Artem Oschepkov <ioshepkov@gmail.com>
2014-08-06 18:18:42 +0400

    [FIX] Short Info Dialog

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/Inventory/reports.js
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/Inventory/reports.js
@@ -1,12 +1,15 @@
-﻿function showComputerShortInfo(id) {
+﻿function applyComputerShortInfoBehavior(parameters) {
+    if (!parameters.url) throw new Error('url must be specified.');
+    window.showComputerShortInfo = function (id) {
 
-    if (!id) {
-        return;
-    }
+        if (!id) {
+            return;
+        }
 
-    $.get('@Url.Action("SearchComputerShortInfo")', { computerId: id }, function (data) {
-        $("#computer_short_info_dialog_container").html(data);
-        $("#computer_short_info_select_div").parent().appendTo("#computer_short_info_dialog_container");
-        $('#computer_short_info_select_div').dialog('open');
-    });
+        $.get(parameters.url, { computerId: id }, function (data) {
+            $("#computer_short_info_dialog_container").html(data);
+            $("#computer_short_info_select_div").parent().appendTo("#computer_short_info_dialog_container");
+            $('#computer_short_info_select_div').dialog('open');
+        });
+    };
 };
