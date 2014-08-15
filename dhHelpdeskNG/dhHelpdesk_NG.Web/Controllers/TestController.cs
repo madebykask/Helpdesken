5646673d2d75e8b47eb7566f54f9d8a86bd2ac1c
Alexey Matveev <alexey.matveev@fastdev.se>
2014-08-15 13:36:44 +0400

    [UPD] Logging.

---
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿namespace DH.Helpdesk.Web.Controllers
+{
+    using System;
+    using DH.Helpdesk.Services.Services;
+    using DH.Helpdesk.Web.Infrastructure;
+
+    public class TestController : BaseController
+    {
+        public TestController(IMasterDataService masterDataService)
+            : base(masterDataService)
+        {
+        }
+
+        public void Error(string message)
+        {
+            throw new Exception(message);
+        }
+    }
+}
