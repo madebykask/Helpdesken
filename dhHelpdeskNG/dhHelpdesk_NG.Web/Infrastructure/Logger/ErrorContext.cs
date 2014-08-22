c929e6a5814bd18c5e48ce815c4a049e9effe072
Alexey Matveev <alexey.matveev@fastdev.se>
2014-08-22 09:31:21 +0400

    [FIX] Logging error.

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Infrastructure/Logger/ErrorContext.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Infrastructure/Logger/ErrorContext.cs
@@ -44,10 +44,21 @@
             res.AppendLine(LineSeparator);
             res.AppendLine(string.Format("Controller: {0}", this.Controller));
             res.AppendLine(string.Format("Action: {0}", this.Action));
-            res.AppendLine(string.Format("UserId: {0}", this.WorkContext.User.UserId));
-            res.AppendLine(string.Format("User: {0}", this.WorkContext.User.UserName));
-            res.AppendLine(string.Format("CustomerId: {0}", this.WorkContext.Customer.CustomerId));
-            res.AppendLine(string.Format("Customer: {0}", this.WorkContext.Customer.CustomerName));
+            if (this.WorkContext != null)
+            {
+                if (this.WorkContext.User != null)
+                {
+                    res.AppendLine(string.Format("UserId: {0}", this.WorkContext.User.UserId));
+                    res.AppendLine(string.Format("User: {0}", this.WorkContext.User.UserName));                    
+                }
+
+                if (this.WorkContext.Customer != null)
+                {
+                    res.AppendLine(string.Format("CustomerId: {0}", this.WorkContext.Customer.CustomerId));
+                    res.AppendLine(string.Format("Customer: {0}", this.WorkContext.Customer.CustomerName));                                    
+                }
+            }
+
             res.AppendLine(LineSeparator);
             return res.ToString();
         }
