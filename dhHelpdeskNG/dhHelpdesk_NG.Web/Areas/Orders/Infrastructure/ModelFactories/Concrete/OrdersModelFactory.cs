8d54e55b76ceef12fe98fc64538e046c72ef7ade
Aleksei Matveev <alexey.matveev@fastdev.se>
2014-11-18 16:29:01 +0400

    [UPD] Orders module. Orders list headers.

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Areas/Orders/Infrastructure/ModelFactories/Concrete/OrdersModelFactory.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Areas/Orders/Infrastructure/ModelFactories/Concrete/OrdersModelFactory.cs
@@ -58,47 +58,89 @@
 
         private static void CreateGeneralHeaders(GeneralFieldSettingsOverview settings, List<GridColumnHeaderModel> headers)
         {
-            
+            CreateHeaderIfNeeded(settings.OrderNumber, GeneralFieldNames.OrderNumber, headers);
+            CreateHeaderIfNeeded(settings.Customer, GeneralFieldNames.Customer, headers);
+            CreateHeaderIfNeeded(settings.Administrator, GeneralFieldNames.Administrator, headers);
+            CreateHeaderIfNeeded(settings.Domain, GeneralFieldNames.Domain, headers);
+            CreateHeaderIfNeeded(settings.OrderDate, GeneralFieldNames.OrderDate, headers);
         }
 
         private static void CreateLogHeaders(LogFieldSettingsOverview settings, List<GridColumnHeaderModel> headers)
         {
-            
+            CreateHeaderIfNeeded(settings.Log, LogFieldNames.Log, headers);
         }
 
         private static void CreateOrdererHeaders(OrdererFieldSettingsOverview settings, List<GridColumnHeaderModel> headers)
         {
-            
+            CreateHeaderIfNeeded(settings.OrdererId, OrdererFieldNames.OrdererId, headers);
+            CreateHeaderIfNeeded(settings.OrdererName, OrdererFieldNames.OrdererName, headers);
+            CreateHeaderIfNeeded(settings.OrdererLocation, OrdererFieldNames.OrdererLocation, headers);
+            CreateHeaderIfNeeded(settings.OrdererEmail, OrdererFieldNames.OrdererEmail, headers);
+            CreateHeaderIfNeeded(settings.OrdererPhone, OrdererFieldNames.OrdererPhone, headers);
+            CreateHeaderIfNeeded(settings.OrdererCode, OrdererFieldNames.OrdererCode, headers);
+            CreateHeaderIfNeeded(settings.Department, OrdererFieldNames.Department, headers);
+            CreateHeaderIfNeeded(settings.Unit, OrdererFieldNames.Unit, headers);
+            CreateHeaderIfNeeded(settings.OrdererAddress, OrdererFieldNames.OrdererAddress, headers);
+            CreateHeaderIfNeeded(settings.OrdererInvoiceAddress, OrdererFieldNames.OrdererInvoiceAddress, headers);
+            CreateHeaderIfNeeded(settings.OrdererReferenceNumber, OrdererFieldNames.OrdererReferenceNumber, headers);
+            CreateHeaderIfNeeded(settings.AccountingDimension1, OrdererFieldNames.AccountingDimension1, headers);
+            CreateHeaderIfNeeded(settings.AccountingDimension2, OrdererFieldNames.AccountingDimension2, headers);
+            CreateHeaderIfNeeded(settings.AccountingDimension3, OrdererFieldNames.AccountingDimension3, headers);
+            CreateHeaderIfNeeded(settings.AccountingDimension4, OrdererFieldNames.AccountingDimension4, headers);
+            CreateHeaderIfNeeded(settings.AccountingDimension5, OrdererFieldNames.AccountingDimension5, headers);
         }
 
         private static void CreateOrderHeaders(OrderFieldSettingsOverview settings, List<GridColumnHeaderModel> headers)
         {
-            
+            CreateHeaderIfNeeded(settings.Property, OrderFieldNames.Property, headers);
+            CreateHeaderIfNeeded(settings.OrderRow1, OrderFieldNames.OrderRow1, headers);
+            CreateHeaderIfNeeded(settings.OrderRow2, OrderFieldNames.OrderRow2, headers);
+            CreateHeaderIfNeeded(settings.OrderRow3, OrderFieldNames.OrderRow3, headers);
+            CreateHeaderIfNeeded(settings.OrderRow4, OrderFieldNames.OrderRow4, headers);
+            CreateHeaderIfNeeded(settings.OrderRow5, OrderFieldNames.OrderRow5, headers);
+            CreateHeaderIfNeeded(settings.OrderRow6, OrderFieldNames.OrderRow6, headers);
+            CreateHeaderIfNeeded(settings.OrderRow7, OrderFieldNames.OrderRow7, headers);
+            CreateHeaderIfNeeded(settings.OrderRow8, OrderFieldNames.OrderRow8, headers);
+            CreateHeaderIfNeeded(settings.Configuration, OrderFieldNames.Configuration, headers);
+            CreateHeaderIfNeeded(settings.OrderInfo, OrderFieldNames.OrderInfo, headers);
+            CreateHeaderIfNeeded(settings.OrderInfo2, OrderFieldNames.OrderInfo2, headers);
         }
 
         private static void CreateOtherHeaders(OtherFieldSettingsOverview settings, List<GridColumnHeaderModel> headers)
         {
-            
+            CreateHeaderIfNeeded(settings.FileName, OtherFieldNames.FileName, headers);
+            CreateHeaderIfNeeded(settings.CaseNumber, OtherFieldNames.CaseNumber, headers);
+            CreateHeaderIfNeeded(settings.Info, OtherFieldNames.Info, headers);
+            CreateHeaderIfNeeded(settings.Status, OtherFieldNames.Status, headers);
         }
 
         private static void CreateProgramHeaders(ProgramFieldSettingsOverview settings, List<GridColumnHeaderModel> headers)
         {
-            
+            CreateHeaderIfNeeded(settings.Program, ProgramFieldNames.Program, headers);
         }
 
         private static void CreateReceiverHeaders(ReceiverFieldSettingsOverview settings, List<GridColumnHeaderModel> headers)
         {
-            
+            CreateHeaderIfNeeded(settings.ReceiverId, ReceiverFieldNames.ReceiverId, headers);
+            CreateHeaderIfNeeded(settings.ReceiverName, ReceiverFieldNames.ReceiverName, headers);
+            CreateHeaderIfNeeded(settings.ReceiverEmail, ReceiverFieldNames.ReceiverEmail, headers);
+            CreateHeaderIfNeeded(settings.ReceiverPhone, ReceiverFieldNames.ReceiverPhone, headers);
+            CreateHeaderIfNeeded(settings.ReceiverLocation, ReceiverFieldNames.ReceiverLocation, headers);
+            CreateHeaderIfNeeded(settings.MarkOfGoods, ReceiverFieldNames.MarkOfGoods, headers);
         }
 
         private static void CreateSupplierHeaders(SupplierFieldSettingsOverview settings, List<GridColumnHeaderModel> headers)
         {
-            
+            CreateHeaderIfNeeded(settings.SupplierOrderNumber, SupplierFieldNames.SupplierOrderNumber, headers);
+            CreateHeaderIfNeeded(settings.SupplierOrderDate, SupplierFieldNames.SupplierOrderDate, headers);
+            CreateHeaderIfNeeded(settings.SupplierOrderInfo, SupplierFieldNames.SupplierOrderInfo, headers);
         }
 
         private static void CreateUserHeaders(UserFieldSettingsOverview settings, List<GridColumnHeaderModel> headers)
         {
-            
+            CreateHeaderIfNeeded(settings.UserId, UserFieldNames.UserId, headers);
+            CreateHeaderIfNeeded(settings.UserFirstName, UserFieldNames.UserFirstName, headers);
+            CreateHeaderIfNeeded(settings.UserLastName, UserFieldNames.UserLastName, headers);
         }
 
         private static void CreateHeaderIfNeeded(
