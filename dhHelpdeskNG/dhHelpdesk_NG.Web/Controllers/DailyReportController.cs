658a6c78f533c73d5abb060fb1f8c063a43a699a
Majid Ghanadi <majidco18@gmail.com>
2014-11-19 11:35:29 +0100

    [ADD] DailyReport modul

---
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Controllers/DailyReportController.cs
@@ -0,0 +1,21 @@
+﻿using DH.Helpdesk.Web.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DH.Helpdesk.Web.Controllers
+{
+    public class DailyReportController : BaseController
+    {
+        //
+        // GET: /DailyReport/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
