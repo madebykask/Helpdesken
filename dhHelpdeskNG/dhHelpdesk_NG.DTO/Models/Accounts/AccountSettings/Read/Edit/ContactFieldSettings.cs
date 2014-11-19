73f2d70013ae1d95cfa0857527c5309de8384dbe
Artem Oschepkov <ioshepkov@gmail.com>
2014-11-19 14:41:06 +0400

    [UPD] Account models

---
+++ dhHelpdeskNG/dhHelpdesk_NG.DTO/Models/Accounts/AccountSettings/Read/Edit/ContactFieldSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace DH.Helpdesk.BusinessData.Models.Accounts.AccountSettings.Read.Edit
+{
+    public sealed class ContactFieldSettings
+    {
+        public ContactFieldSettings(FieldSettingMultipleChoices ids, FieldSetting name, FieldSetting phone, FieldSetting email)
+        {
+            this.Ids = ids;
+            this.Name = name;
+            this.Phone = phone;
+            this.Email = email;
+        }
+
+        public FieldSettingMultipleChoices Ids { get; private set; }
+
+        public FieldSetting Name { get; private set; }
+
+        public FieldSetting Phone { get; private set; }
+
+        public FieldSetting Email { get; private set; }
+    }
+}
---
+++ dhHelpdeskNG/dhHelpdesk_NG.DTO/Models/Accounts/AccountSettings/Read/Edit/FieldSettingMultipleChoices.cs
@@ -0,0 +1,19 @@
+﻿namespace DH.Helpdesk.BusinessData.Models.Accounts.AccountSettings.Read.Edit
+{
+    public class FieldSettingMultipleChoices : FieldSetting
+    {
+        public FieldSettingMultipleChoices(
+            bool showInDetails,
+            bool showInList,
+            string caption,
+            string help,
+            bool isRequired,
+            bool isMultiple)
+            : base(showInDetails, showInList, caption, help, isRequired)
+        {
+            this.IsMultiple = isMultiple;
+        }
+
+        public bool IsMultiple { get; private set; }
+    }
+}
