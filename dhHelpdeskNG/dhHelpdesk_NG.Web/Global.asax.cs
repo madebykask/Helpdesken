9304c3593e3c02d99964a128458039064de36ebe
Majid Ghanadi <majidco18@gmail.com>
2014-12-03 11:22:25 +0100

    [FIX] Routing Map

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Global.asax.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Global.asax.cs
@@ -34,6 +34,7 @@
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" });
 
             routes.MapRoute(
                 "Default",
