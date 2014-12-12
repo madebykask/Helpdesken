891c28a0ab9120df9f5c103a8d501e3eb18733a5
dh_majid_ghanadi <majid.ghanadi@dhsolutions.se>
2014-12-12 10:18:09 +0100

    [FIX] initiatore setting

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/Notifiers/settings.js
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/Notifiers/settings.js
@@ -1,6 +1,6 @@
 ﻿function applySettingsViewBehavior(parameters) {
     $('#language_dropdown').change(function() {
-        $.get(parameters.settingsUrl, { languageId: $(this).val() }, function(settingsMarkup) {
+        $.get(parameters.settingsUrl, { languageId: $(this).val(), now: Date.now() }, function (settingsMarkup) {
             $('#settings_container').html(settingsMarkup);
         });
     });
