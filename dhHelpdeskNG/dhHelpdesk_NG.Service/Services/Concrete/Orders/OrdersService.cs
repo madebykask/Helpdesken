a7e33b9233adaccdf432ba4fc93b634efe263fac
Aleksei Matveev <alexey.matveev@fastdev.se>
2014-12-17 09:28:40 +0400

    [FIX] Orders module. Deleting order.

--- dhHelpdeskNG/dhHelpdesk_NG.Service/Services/Concrete/Orders/OrdersService.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Service/Services/Concrete/Orders/OrdersService.cs
@@ -206,10 +206,15 @@
             using (var uow = this.unitOfWorkFactory.Create())
             {
                 var ordersRep = uow.GetRepository<Order>();
+                var orderHistoryRep = uow.GetRepository<OrderHistoryEntity>();
+
                 var order = ordersRep.GetById(id);
                 order.Logs.Clear();
                 order.Programs.Clear();
-                order.Histories.Clear();
+
+                var orderHistoryIds = order.Histories.Select(h => h.Id);
+                orderHistoryRep.DeleteWhere(h => orderHistoryIds.Contains(h.Id));
+
                 ordersRep.DeleteById(id);
 
                 uow.Save();
