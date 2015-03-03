770467b7ae2c5ef84f1355657b6e1d5cb2c8a9e5
dh_majid_ghanadi <majid.ghanadi@dhsolutions.se>
2015-03-03 15:49:10 +0100

    [FIX] scripts

--- Database/SQL scripts/2015-03-02 - tblCustomer_Selfservice setting.sql
+++ Database/SQL scripts/2015-03-02 - tblCustomer_Selfservice setting.sql
@@ -1,5 +1,5 @@
 IF COL_LENGTH('dbo.tblCustomer','ShowFAQOnExternalStartPage') IS NULL
 BEGIN
-	ALTER TABLE [dbo].tblCustomer ADD ShowFAQOnExternalStartPage int not
+	ALTER TABLE [dbo].tblCustomer ADD ShowFAQOnExternalStartPage int not default (0)
 END
 GO
