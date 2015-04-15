60d52d3553411764a428b99aee8017c4925a6fbe
Majid Ghanadi <majidco18@gmail.com>
2015-04-15 10:27:05 +0200

    [ADD] SharedAssemblyInfo to have same version for all projects

--- dhHelpdeskNG/dhHelpdesk_NG.Data/Properties/AssemblyInfo.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Data/Properties/AssemblyInfo.cs
@@ -1,9 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCompany("DH Solutions")]
+
 [assembly: AssemblyTitle("DH.Helpdesk.Dal")]
-[assembly: AssemblyCopyright("© 2014 DH Solutions")]
-[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: Guid("82a871c5-7799-455a-819f-0a8d26f1090b")]
 [assembly: ComVisible(false)]
--- dhHelpdeskNG/dhHelpdesk_NG.Domain/Properties/AssemblyInfo.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Domain/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCompany("DH Solutions")]
 [assembly: AssemblyTitle("DH.Helpdesk.Domain")]
-[assembly: AssemblyCopyright("© 2014 DH Solutions")]
-[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: Guid("2d09f66a-d158-417f-94f6-9e7b20dd7e82")]
 [assembly: ComVisible(false)]
