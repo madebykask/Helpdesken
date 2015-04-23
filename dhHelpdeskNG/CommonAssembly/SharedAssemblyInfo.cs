c60e61b401041d51d1749405476a6379a3ba8d10
Alexander Semenischev <alexander.semenischev@fastdev.se>
2015-04-23 14:21:37 +0200

    [UPD] prepare to 5.3.6 release

--- dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
+++ dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("DH Solutions")]
 [assembly: AssemblyCopyright("© 2015 DH Solutions")]
-[assembly: AssemblyVersion("5.3.6.1")]
-[assembly: AssemblyFileVersionAttribute("5.3.6.1")]
+[assembly: AssemblyVersion("5.3.7.0")]
+[assembly: AssemblyFileVersionAttribute("5.3.7.0")]
