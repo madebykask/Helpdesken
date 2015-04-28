41dc6bd5bf676eb074bbc3fe88ebe5a5f3b0194c
Alexander Semenischev <alexander.semenischev@fastdev.se>
2015-04-28 13:46:41 +0200

    [FIX] DB fix for old field name in case overview table

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -1,5 +1,8 @@
 -- update DB from 5.3.6.XX to 5.3.7.xx version
 
+-- fix for old field names, we have discovered this bug on production 
+-- 2015-04-28
+UPDATE tblCaseSettings SET tblCaseName = Replace(tblCaseName, 'tblUsers.FirstName', 'Performer_User_Id')
 
 
 -- Last Line to update database version
