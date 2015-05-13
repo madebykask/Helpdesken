14ed0d895c1988e13cd5b7fae58a8b8ffe4f7857
Alexander Semenischev <alexander.semenischev@fastdev.se>
2015-05-13 15:35:41 +0200

    [UPD] update dev env

--- dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
+++ dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyCompany("DH Solutions")]
 [assembly: AssemblyCopyright("© 2015 DH Solutions")]
-[assembly: AssemblyVersion("5.3.7.4")]
-[assembly: AssemblyFileVersionAttribute("5.3.7.4")]
+[assembly: AssemblyVersion("5.3.7.5")]
+[assembly: AssemblyFileVersionAttribute("5.3.7.5")]
