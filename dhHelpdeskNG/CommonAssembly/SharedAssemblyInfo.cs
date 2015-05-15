d43f3d70119597d2f308a971ad4220580ee80af8
Majid Ghanadi <majidco18@gmail.com>
2015-05-15 11:13:08 +0200

    [UPD] version acct

--- dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
+++ dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyCompany("DH Solutions")]
 [assembly: AssemblyCopyright("© 2015 DH Solutions")]
-[assembly: AssemblyVersion("5.3.6.6")]
-[assembly: AssemblyFileVersionAttribute("5.3.6.6")]
+[assembly: AssemblyVersion("5.3.6.7")]
+[assembly: AssemblyFileVersionAttribute("5.3.6.7")]
