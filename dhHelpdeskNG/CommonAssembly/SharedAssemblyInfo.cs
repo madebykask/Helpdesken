3b6d7e3a6fae68017ea114ccd2a699058e4c1be6
Majid Ghanadi <majidco18@gmail.com>
2015-05-18 14:14:29 +0200

    [UPD] hide incomplete modules

--- dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
+++ dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("DH Solutions")]
 [assembly: AssemblyCopyright("© 2015 DH Solutions")]
-[assembly: AssemblyVersion("5.3.7.7")]
-[assembly: AssemblyFileVersionAttribute("5.3.7.7")]
+[assembly: AssemblyVersion("5.3.7.8")]
+[assembly: AssemblyFileVersionAttribute("5.3.7.8")]
