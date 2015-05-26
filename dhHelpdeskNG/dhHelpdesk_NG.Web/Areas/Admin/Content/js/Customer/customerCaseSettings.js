bbce6527c9bf70a84d9e72e838900db0fd3cd6b7
Alexander Semenischev <alexander.semenischev@fastdev.se>
2015-05-26 12:09:26 +0200

    [FIX] #12081 Can save 'case' in administration with a field only checked as 'Show on self-service'

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Areas/Admin/Content/js/Customer/customerCaseSettings.js
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Areas/Admin/Content/js/Customer/customerCaseSettings.js
@@ -7,9 +7,12 @@ $(function () {
     $(input + properties.ShowOnStartPage).click(function () {
         var elementName = this.name.replace(properties.ShowOnStartPage, properties.ShowExternal);
         var curElement = jQuery('[name="' + elementName + '"]');
+        console.log('catch me');
+        var $requiredElement = $(this).parents('tr').find('[name$="Required"]:checkbox').not(':disabled');
         var visibleElement = curElement.filter(':visible').get(0);
         if (this.checked == false && visibleElement != undefined) {
             visibleElement.checked = false;
+            $requiredElement.prop('checked', false);
         }
     });
 
@@ -21,7 +24,15 @@ $(function () {
             this.checked = false;
         }
     });
-   
+
+    $('input[name$=".Required"]').on('click', function (ev) {
+        var $mainEl = $(this).parents('tr').find('[name$=".ShowOnStartPage"]:checkbox');
+        if (!$mainEl.prop('checked')) {
+            ev.preventDefault();
+            return false;
+        }
+        return true;
+    });
 });
 
 $(function () {    
