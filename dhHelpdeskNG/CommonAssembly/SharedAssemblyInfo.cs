336031d3e69d74738b018f2efad393372bd14c0d
Alexander Semenischev <alexander.semenischev@fastdev.se>
2015-05-28 08:33:27 +0200

    [UPD] updating dev environment

--- dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
+++ dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("DH Solutions")]
 [assembly: AssemblyCopyright("© 2015 DH Solutions")]
-[assembly: AssemblyVersion("5.3.8.3")]
-[assembly: AssemblyFileVersionAttribute("5.3.8.3")]
+[assembly: AssemblyVersion("5.3.8.4")]
+[assembly: AssemblyFileVersionAttribute("5.3.8.4")]
--- dhHelpdeskNG/dhHelpdesk_NG.Mobile/Content/js/dhHelpdesk.js
+++ dhHelpdeskNG/dhHelpdesk_NG.Mobile/Content/js/dhHelpdesk.js
@@ -1339,16 +1339,3 @@ $('.switchcheckbox').bootstrapSwitch('onColor', 'success');
 
 
 $(".chosen-select").chosen({ width: "300px" });
-
-//TIMYMCE
-//tinymce.init({
-//    selector: "textarea.richtexteditor",
-//    plugins: [
-//        "advlist autolink lists link image charmap print preview anchor",
-//        "searchreplace visualblocks code fullscreen",
-//        "insertdatetime media table contextmenu paste charmap"
-//    ],
-//    menubar: false,
-//    //toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
-//    toolbar: "undo redo | removeformat | fontsizeselect bold italic | bullist numlist | link unlink | charmap | code"
-//});
