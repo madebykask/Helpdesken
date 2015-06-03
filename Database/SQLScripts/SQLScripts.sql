9a043e58681a03fb6ffbe144391caca46e2bab2e
Majid Ghanadi <majidco18@gmail.com>
2015-06-03 16:20:19 +0200

    [UPD] new script version

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -1,14 +1,6 @@
--- update DB from 5.3.7.XX to 5.3.8.xx version
+-- update DB from 5.3.8.XX to 5.3.9.xx version
 
--- 2015-05-18
-update tblUsers set timezoneid = 'W. Europe Standard Time' where timezoneid is null;
-go
 
-if not exists (select * from tblTextType where id = 300)
-begin
-  insert into tblTextType values(300, 'Line Manager', 1)
-end
-GO
 
 -- Last Line to update database version
-UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.8'
+UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.9'
