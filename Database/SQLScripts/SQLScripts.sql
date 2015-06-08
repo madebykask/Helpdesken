134449313328aed737ad5fd2a06d7b3542cb5a53
Alexander Semenischev <alexander.semenischev@fastdev.se>
2015-06-08 11:56:31 +0200

    [UPD] change "alter table" to one-off execution, added SQL code to append new field to all existing customers

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -3,7 +3,25 @@
 ALTER TABLE tblSettings ALTER COLUMN LDAPFilter nvarchar(100) NOT NULL
 
 -- Temporary solution for "Source" field
-ALTER TABLE tblCase ADD source_id int not Null default 1;
+IF COL_LENGTH('dbo.tblCase','source_id') IS NULL
+BEGIN
+	ALTER TABLE tblCase ADD source_id int not Null default 1;
+END
+go
+
+set ansi_nulls off;
+insert into tblCaseFieldSettings(customer_id, casefield, show, required) 
+select t1.* from (
+	select distinct customer_id, 'source' as source, '0' as show, '0' as required
+	from tblCaseFieldSettings
+) as t1
+where t1.Customer_Id not in (
+	select customer_id 
+	from tblCaseFieldSettings
+	where CaseField = 'source'
+)
+order by customer_id
+set ansi_nulls on;
 
 -- Last Line to update database version
 UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.9'
