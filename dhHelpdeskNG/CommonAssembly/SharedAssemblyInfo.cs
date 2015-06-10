356028e50f0c844f23e02a8afe894d70b7393936
Majid Ghanadi <majidco18@gmail.com>
2015-06-10 12:21:26 +0200

    [UPD] version

--- dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
+++ dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("DH Solutions")]
 [assembly: AssemblyCopyright("© 2015 DH Solutions")]
-[assembly: AssemblyVersion("5.3.9.2")]
-[assembly: AssemblyFileVersionAttribute("5.3.9.2")]
+[assembly: AssemblyVersion("5.3.9.3")]
+[assembly: AssemblyFileVersionAttribute("5.3.9.3")]
