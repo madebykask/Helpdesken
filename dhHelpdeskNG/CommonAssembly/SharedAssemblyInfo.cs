edfb31c91a40296a442b94ce34e5e84fbd1a15b5
Alexander Semenischev <alexander.semenischev@fastdev.se>
2015-06-11 16:30:58 +0200

    [UPD] update DEV environment

--- dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
+++ dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Reflection;
 
 [assembly: AssemblyCompany("DH Solutions")]
 [assembly: AssemblyCopyright("© 2015 DH Solutions")]
-[assembly: AssemblyVersion("5.3.9.3")]
-[assembly: AssemblyFileVersionAttribute("5.3.9.3")]
+[assembly: AssemblyVersion("5.3.9.4")]
+[assembly: AssemblyFileVersionAttribute("5.3.9.4")]
