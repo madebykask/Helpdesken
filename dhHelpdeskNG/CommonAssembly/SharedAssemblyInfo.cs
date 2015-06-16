c74ead670bda1c653a42b059c939807bd3574555
Alexander Semenischev <alexander.semenischev@fastdev.se>
2015-06-16 11:21:27 +0200

    [UPD] ACCT environment version

--- dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
+++ dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyCompany("DH Solutions")]
 [assembly: AssemblyCopyright("© 2015 DH Solutions")]
-[assembly: AssemblyVersion("5.3.9.12")]
-[assembly: AssemblyFileVersionAttribute("5.3.9.12")]
+[assembly: AssemblyVersion("5.3.9.13")]
+[assembly: AssemblyFileVersionAttribute("5.3.9.13")]
