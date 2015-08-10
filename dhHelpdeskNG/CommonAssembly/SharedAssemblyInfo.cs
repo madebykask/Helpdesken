6858ce61f4eeb21f0bf386d7756c158efaed00f0
Alexander Semenischev <alexander.semenischev@fastdev.se>
2015-08-10 09:40:17 +0200

    [UPD] new deploy on ACCT

--- dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
+++ dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyCompany("DH Solutions")]
 [assembly: AssemblyCopyright("© 2015 DH Solutions")]
-[assembly: AssemblyVersion("5.3.11.7")]
-[assembly: AssemblyFileVersionAttribute("5.3.11.7")]
+[assembly: AssemblyVersion("5.3.11.8")]
+[assembly: AssemblyFileVersionAttribute("5.3.11.8")]
