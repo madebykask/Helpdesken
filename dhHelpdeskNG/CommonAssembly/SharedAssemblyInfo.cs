7cac662e438d0ec8748359f1757c55b8a841e678
Alexander Semenischev <alexander.semenischev@fastdev.se>
2015-08-11 11:01:08 +0200

    [UPD] version update on ACCT

--- dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
+++ dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyCompany("DH Solutions")]
 [assembly: AssemblyCopyright("© 2015 DH Solutions")]
-[assembly: AssemblyVersion("5.3.11.13")]
-[assembly: AssemblyFileVersionAttribute("5.3.11.13")]
+[assembly: AssemblyVersion("5.3.11.14")]
+[assembly: AssemblyFileVersionAttribute("5.3.11.14")]
