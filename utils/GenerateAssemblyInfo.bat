968be5e77026814a27ca304c61f86220dbee4d7f
Alexander Semenischev <alexander.semenischev@fastdev.se>
2015-08-19 16:55:29 +0200

    [UPD] small changes in build script;
    [UPD] version format changed

--- utils/GenerateAssemblyInfo.bat
+++ utils/GenerateAssemblyInfo.bat
@@ -3,4 +3,4 @@
 :: where FILE_NAME is where to put generated content for AssemblyInfo.cs
 :: HINT: for more verbose MSBuild output add /v:d parameter
 echo =========== resolving version from GIT ======================
-%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe %~dp0\GenerateAssemblyInfo.xml /p:AssemblyFileName=%1
+%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe "%~dp0\GenerateAssemblyInfo.xml" /p:AssemblyFileName=%1
