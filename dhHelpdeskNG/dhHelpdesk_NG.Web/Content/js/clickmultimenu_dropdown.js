d089b87be726e591313a2c978db806bf099cb604
Alexander Semenischev <alexander.semenischev@fastdev.se>
2015-09-03 17:14:42 +0200

    [UPD] WIP on dropdown

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/clickmultimenu_dropdown.js
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/clickmultimenu_dropdown.js
@@ -1,16 +1,19 @@
 ﻿"use strict";
 
-$('.clickmultimenu .dropdown-submenu').click(function () {
+$('.clickmultimenu .dropdown-submenu').click(function (ev) {
     var isChildSubMenuVisible = $(this).children('.dropdown-menu').css('display') == 'block';
-    var submenus = $(this).find('.dropdown-submenu');
-    debugger
+    var clickedOn = ev.target;
+    var isHasSubMenu = $(clickedOn).nextAll('.dropdown-menu').length !== 0;
+    var $container = $(this).parents('.clickmultimenu:first');
     if (isChildSubMenuVisible) {
-        $(this).find('.dropdown-menu').css('display', 'none');
-
+        if (!isHasSubMenu) {
+            $container.dropdown('toggle');
+        } else {
+            $(this).find('.dropdown-menu').css('display', 'none');
+        }
     } else {
         $(this).parent().find('.dropdown-menu').css('display', 'none');
         $(this).children('.dropdown-menu').css('display', 'block');
     }
-
     return false;
 });
