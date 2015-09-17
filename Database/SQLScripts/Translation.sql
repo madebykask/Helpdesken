4742d858dd86869d187b2c3f767fd8b0fa56344b
Alexander Semenischev <alexander.semenischev@fastdev.se>
2015-09-17 08:38:47 +0200

    [FIX] translation for parent-child task

--- Database/SQLScripts/Translation.sql
+++ Database/SQLScripts/Translation.sql
@@ -4502,6 +4502,14 @@ If not exists (select * from tblTextTranslation where text_id = 1547 and Languag
 	insert into tblTextTranslation(Text_Id, Language_Id, TextTranslation) VALUES(1547, 2, 'Child case')
 GO
 
+If not exists (select * from tbltext where id = 1548)
+	insert into tbltext (id, TextString) VALUES (1548, 'Skapa underärende')
+GO
+If not exists (select * from tblTextTranslation where text_id = 1548 and Language_Id = 2)
+	insert into tblTextTranslation(Text_Id, Language_Id, TextTranslation) VALUES(1548, 2, 'Create child case')
+GO
+
+
 
 -- *** Run this last when put translation script above this line **--
 update tblTextTranslation set CreatedDate = GETDATE(), ChangedDate  = GETDATE() where CreatedDate is null
