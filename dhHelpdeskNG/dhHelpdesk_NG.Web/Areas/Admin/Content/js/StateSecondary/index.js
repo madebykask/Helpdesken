0da2ea1efe10b8bcdb18cc672ca1bf8251398897
dh_soniya_ghorbani <soniya.ghorbani@dhsolutions.se>
2015-09-18 09:32:50 +0200

    [FIX] #52674 Substatus Session

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Areas/Admin/Content/js/StateSecondary/index.js
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Areas/Admin/Content/js/StateSecondary/index.js
@@ -4,7 +4,7 @@
 $(document).ready(function () {
     var stateSecondaryList = new ToggableInactiveList();
     stateSecondaryList.init({
-        saveStateUrl: 'StateSecondary/ShowOnlyActiveStateSecondariesInAdmin',
+        saveStateUrl: 'StateSecondary/SetShowOnlyActiveStateSecondariesInAdmin',
         $baseContainer: $('.tab-pane').first()
     });
 });
