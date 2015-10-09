1190ffff99a9a37877499621728a744656e93728
Majid Ghanadi <majidco18@gmail.com>
2015-10-09 15:33:51 +0200

    [FIX] Show error message if action is not defined

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Controllers/ErrorController.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Controllers/ErrorController.cs
@@ -5,14 +5,15 @@
 
     using DH.Helpdesk.Services.Services;
     using DH.Helpdesk.Web.Infrastructure;
+    using DH.Helpdesk.Common.Enums;
 
     public class ErrorController : Controller
-    {
-        private readonly ITextTranslationService _textTranslationService;
+    {        
+        private readonly IMasterDataService _masterDataService;
 
-        public ErrorController(ITextTranslationService textTranslationService)
+        public ErrorController(IMasterDataService masterDataService)
         {
-            this._textTranslationService = textTranslationService;
+            this._masterDataService = masterDataService; 
         }
 
         public ActionResult Index()
@@ -21,18 +22,53 @@
         }
 
         public ActionResult NotFound()
-        {
-            
+        {            
             return this.View("Error");
         }
 
         public ActionResult NotFound404()
         {
-            /// 999 is for errorcodes
-            var text = _textTranslationService.GetAllTexts(999,null).Where(x=>x.TextToTranslate == "404 Error").FirstOrDefault();
+            var errorMessage = "Error 404: Page was not found!";
+            var curLanguageId = GetMessageLanguageId();
+
+            if (curLanguageId > 0)
+            {                
+                var text = this._masterDataService.GetTranslationTexts()
+                                                  .Where(t => t.Type == TranslationType.ErrorText && t.TextToTranslate == "404 Error")
+                                                  .FirstOrDefault();
+                if (text != null)
+                {
+                    var textForCurrentLang = text.TextTranslations.Where(tt => tt.Language_Id == curLanguageId).FirstOrDefault();
+                    if (textForCurrentLang != null)
+                        errorMessage = textForCurrentLang.TextTranslated;
+                    else
+                    {
+                        var firstTranslation = text.TextTranslations.FirstOrDefault();
+                        if (firstTranslation != null)
+                            errorMessage = firstTranslation.TextTranslated;
+                    }
+                }
+            }
+
+            ViewBag.ErrorMessage = errorMessage;
             Response.StatusCode = 404;
-            ViewBag.ErrorMessage = text.TextTranslated;
             return this.View();
+        }
+
+        private int GetMessageLanguageId()
+        {
+            var ret = 0;
+            var globalSetting = this._masterDataService.GetGlobalSettings().FirstOrDefault();
+
+            if (globalSetting != null)
+            {                
+                if (SessionFacade.CurrentLanguageId > 0)
+                    ret = SessionFacade.CurrentLanguageId;
+                else
+                    ret = globalSetting.DefaultLanguage_Id;
+            }
+
+            return ret;
         }
 
         public ViewResult BusinessLogicError()
