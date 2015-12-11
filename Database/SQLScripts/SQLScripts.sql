7bf4be442f2ae493aecf79d65ca2e659317830f1
Majid Ghanadi <majidco18@gmail.com>
2015-12-11 09:03:46 +0100

    [ADD] new version script places

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -1,15 +1,7 @@
--- update DB from 5.3.17 to 5.3.18 version
+-- update DB from 5.3.18 to 5.3.19 version
 
-ALTER TABLE tblEMailLog ALTER COLUMN ResponseMessage nvarchar(1000)
-GO
-
-IF COL_LENGTH('dbo.tblstatesecondary','RecalculateWatchDate') IS NULL
-BEGIN 	 
-	ALTER TABLE [dbo].tblstatesecondary ADD [RecalculateWatchDate] int not null default(0)
-END
-Go
 
 
 -- Last Line to update database version
-UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.18'
+UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.19'
 
---
+++ Database/SQLScripts/ScriptVersions/v5.3.18.xx.sql
@@ -0,0 +1,15 @@
+-- update DB from 5.3.17 to 5.3.18 version
+
+ALTER TABLE tblEMailLog ALTER COLUMN ResponseMessage nvarchar(1000)
+GO
+
+IF COL_LENGTH('dbo.tblstatesecondary','RecalculateWatchDate') IS NULL
+BEGIN 	 
+	ALTER TABLE [dbo].tblstatesecondary ADD [RecalculateWatchDate] int not null default(0)
+END
+Go
+
+
+-- Last Line to update database version
+UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.18'
+
