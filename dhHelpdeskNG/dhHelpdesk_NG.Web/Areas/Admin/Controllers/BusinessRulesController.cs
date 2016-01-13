dee3a1489cadb4f1c67262da7baf862ee188f9d2
MR <romanyuk_m@outlook.com>
2016-01-13 12:12:50 +0300

    added Business rules page

---
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Areas/Admin/Controllers/BusinessRulesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+using DH.Helpdesk.Domain;
+using DH.Helpdesk.Domain.Changes;
+using DH.Helpdesk.Services.Services;
+using DH.Helpdesk.Web.Areas.Admin.Models;
+using DH.Helpdesk.Web.Infrastructure;
+
+namespace DH.Helpdesk.Web.Areas.Admin.Controllers
+{
+    public class BusinessRulesController : BaseAdminController
+    {
+        public BusinessRulesController(IMasterDataService masterDataService) 
+            : base(masterDataService)
+        {
+            
+        }
+
+        public ActionResult Index(int customerId)
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id, int customerId)
+        {
+            return View();
+        }
+    }
+}
