8936ca31b0ca498334de9a2f5068aa2a44c96a50
Majid Ghanadi <majidco18@gmail.com>
2016-01-26 09:16:11 +0100

    [FIX] #53788 Error message for check inactive data

--- Database/SQLScripts/Translation.sql
+++ Database/SQLScripts/Translation.sql
@@ -4224,5 +4224,12 @@ If not exists (select * from tblTextTranslation where Text_Id = 1577 and Languag
             insert into tblTextTranslation (Text_Id, TextTranslation, Language_Id) VALUES (1577, 'You can not delete this lognote, because you have no permission to delete attached files', 2)
 GO
 
+If not exists (select * from tblText where Id = 1580)
+            insert into tblText (Id, Textstring) VALUES (1580, 'Problem med kommunikation med servern.')
+GO
+If not exists (select * from tblTextTranslation where Text_Id = 1580 and Language_Id=2)
+            insert into tblTextTranslation (Text_Id, TextTranslation, Language_Id) VALUES (1580, 'Problem with Connection to server.', 2)
+GO
+
 -- *** Run this last when put translation script above this line **--
 update tblTextTranslation set CreatedDate = GETDATE(), ChangedDate  = GETDATE() where CreatedDate is null
