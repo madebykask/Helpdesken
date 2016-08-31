9af51f33dddedbe0acdcfd3c7cfd4e1e4f1ef210
dh_catharina_gunnarsson <cg@dhsolutions.se>
2016-08-31 14:50:57 +0200

    [ADD] article - ProductAreas search

---
+++ dhHelpdeskNG/dhHelpdesk_NG.DTO/Models/Invoice/InvoiceArticleProductAreaSelectedFilter.cs
@@ -0,0 +1,20 @@
+﻿namespace DH.Helpdesk.BusinessData.Models.Invoice
+{
+    using DH.Helpdesk.BusinessData.Models.Shared;
+    using System.Collections.Generic;
+
+    public class InvoiceArticleProductAreaSelectedFilter
+    {
+        public InvoiceArticleProductAreaSelectedFilter()
+        { 
+            this.SelectedProductAreas = new SelectedItems();
+            this.SelectedInvoiceArticles = new SelectedItems();
+           
+        }
+
+        public SelectedItems SelectedProductAreas { get; set; }
+
+        public SelectedItems SelectedInvoiceArticles { get; set; }
+
+    }
+}
