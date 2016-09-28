12fde82d944729a2542cec7f973bd02eea0569ea
Majid Ghanadi <majidco18@gmail.com>
2016-09-28 11:25:05 +0200

    [UPD] BR static page

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Areas/Admin/Controllers/BusinessRuleController.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Areas/Admin/Controllers/BusinessRuleController.cs
@@ -5,37 +5,144 @@ using System.Web;
 using System.Web.Mvc;
 using DH.Helpdesk.Services.Services;
 using DH.Helpdesk.Web.Areas.Admin.Models.BusinessRule;
+using DH.Helpdesk.Domain;
+using DH.Helpdesk.Web.Infrastructure.Extensions;
+using DH.Helpdesk.Web.Infrastructure;
 
 namespace DH.Helpdesk.Web.Areas.Admin.Controllers
 {
     public class BusinessRuleController : BaseAdminController
     {
         private readonly ICustomerService _customerService;
+        private readonly IProductAreaService _productAreaService;
+        private readonly IStateSecondaryService _subStatusService;
+        private readonly IMailTemplateService _mailTemplateService;
+        private readonly IEmailGroupService _emailGroupService;
 
         public BusinessRuleController(IMasterDataService masterDataService,
-                                      ICustomerService customerService)
+                                      ICustomerService customerService,
+                                      IProductAreaService productAreaService,
+                                      IStateSecondaryService subStatusService,
+                                      IMailTemplateService mailTemplateService,
+                                      IEmailGroupService emailGroupService
+                                     )
             : base(masterDataService)
         {
             _customerService = customerService;
+            _productAreaService = productAreaService;
+            _subStatusService = subStatusService;
+            _mailTemplateService = mailTemplateService;
+            _emailGroupService = emailGroupService;
         }
 
         #region Public Methods 
         
         [HttpGet]
-        public ActionResult Index(int customerId, int moduleId)
+        public ActionResult Index(int customerId)
         {
             var model = new BusinessRuleIndexModel();
-            model.Customer = _customerService.GetCustomer(customerId);
-            model.Module = new BRModuleModel(moduleId);               
+            model.Customer = _customerService.GetCustomer(customerId);            
             return View(model);
         }
 
         [HttpGet]
-        public ActionResult NewRule(int customerId, int moduleId)
+        public ActionResult NewRule(int customerId)
         {
-            var model = new BusinessRuleIndexModel();
-            model.Customer = _customerService.GetCustomer(customerId);
-            model.Module = new BRModuleModel(moduleId);
+            var model = new BusinessRuleInputModel();
+            model.CustomerId = customerId;
+            model.RuleName = string.Empty;
+            model.Events = new List<BREvent>();
+            model.Events.Add(new BREvent(1, "On Save Case", true));
+            model.ContinueOnSuccess = true;
+            model.ContinueOnError = true;
+            model.IsActive = true;
+
+            var fieldItems = new List<SelectListItem>();
+
+            fieldItems.Add(new SelectListItem(){ Value= "-1", Text = "", Selected = true});
+            fieldItems.Add(new SelectListItem(){ Value= "0", Text = "[Null]", Selected = false});
+            fieldItems.Add(new SelectListItem(){ Value= int.MaxValue.ToString(), Text = "[Any]", Selected = false});
+            
+            var productAreas = _productAreaService.GetAll(customerId);
+            var lastLevels = new List<ProductArea>();
+
+            foreach (var p in productAreas)
+            {
+                if (p.SubProductAreas.Count == 0 && p.IsActive != 0)
+                {
+                    p.Name = p.ResolveFullName();
+                    lastLevels.Add(p);
+                }
+            }  
+
+            var processList = lastLevels.Select(l=> new SelectListItem()
+                                                { 
+                                                    Value= l.Id.ToString(), 
+                                                    Text = l.Name, 
+                                                    Selected = false
+                                                }).ToList();
+
+            var subStatusList = _subStatusService.GetActiveStateSecondaries(customerId).Select(s => new SelectListItem()
+            {
+                Value = s.Id.ToString(),
+                Text = s.Name,
+                Selected = false
+            }).ToList();
+
+            model.Condition = new BRConditionModel()
+            {
+                Id = 0,
+                RuleId = 0,
+                Process = processList,
+                ProcessFromValue = new List<SelectListItem>(),
+                ProcessToValue = new List<SelectListItem>(),
+
+                SubStatus = subStatusList,
+                SubStatusFromValue = new List<SelectListItem>(),
+                SubStatusToValue = new List<SelectListItem>(),                
+
+                Sequence = 1                
+            };
+
+            var customMailTemplates = _mailTemplateService.GetCustomMailTemplates(customerId).ToList();
+            var activeMailTemplates = customMailTemplates.Where(m => m.TemplateLanguages
+                                                                    .Where(tl => !string.IsNullOrEmpty(tl.Subject) && !string.IsNullOrEmpty(tl.Body)).Any() &&
+                                                                    m.MailId >= 100
+                                                               )
+                                                         .ToList();
+
+            var emailTemplateList = new List<SelectListItem>();
+            foreach (var mailtemplate in activeMailTemplates)
+            {
+                var templateId = mailtemplate.MailTemplateId;
+                var templateName = string.Empty;
+                var activeLanguages = mailtemplate.TemplateLanguages.Where(l => !string.IsNullOrEmpty(l.Subject) && !string.IsNullOrEmpty(l.Body)).ToList();
+                if (activeLanguages.Select(l => l.LanguageId).Contains(SessionFacade.CurrentLanguageId))
+                    templateName = activeLanguages.Where(l => l.LanguageId == SessionFacade.CurrentLanguageId).First().TemplateName;
+                else
+                    templateName = activeLanguages.First().TemplateName;
+
+                emailTemplateList.Add(new SelectListItem { Value = templateId.ToString(), Text = templateName, Selected = false });
+            }
+
+            var emailGroupList = _emailGroupService.GetEmailGroups(customerId)
+                                                   .Select(mg => new SelectListItem
+                                                        {
+                                                            Value = mg.Id.ToString(),
+                                                            Text = mg.Name,
+                                                            Selected = false
+                                                        }).ToList();
+            model.Action = new BRActionModel()
+            {
+                Id = 0,
+                RuleId = 0,
+                //Send Email
+                ActionTypeId = 1,
+                EMailTemplates = emailTemplateList,
+                EMailGroups = emailGroupList,
+                Sequence = 1
+            };
+            
             return View(model);
         }
 
---
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Areas/Admin/Models/BusinessRule/BusinessRuleInputModel.cs
@@ -0,0 +1,99 @@
+﻿using DH.Helpdesk.Common.Enums.BusinessRule;
+using DH.Helpdesk.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DH.Helpdesk.Web.Areas.Admin.Models.BusinessRule
+{
+    public class BusinessRuleInputModel
+    {
+        public BusinessRuleInputModel()
+        {
+        }
+
+        public int CustomerId { get; set; }
+
+        public string RuleName { get; set; }
+
+        public List<BREvent> Events { get; set; }
+
+        public int Sequence { get; set; }
+
+        public bool ContinueOnSuccess { get; set; }
+
+        public bool ContinueOnError { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public BRConditionModel Condition { get; set; }
+
+        public BRActionModel Action { get; set; }
+
+        
+    }
+
+    public class BREvent
+    {
+        public BREvent(int id, string name, bool selected)
+        {
+            Id = id;
+            Name = name;
+            Selected = selected;
+        }
+
+        public int Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public bool Selected { get; private set; }
+    }
+
+    public class BRConditionModel
+    {
+        public BRConditionModel()
+        {
+
+        }
+
+        public int Id { get; set; }
+
+        public int RuleId { get; set; }
+
+        public List<SelectListItem> Process { get; set; }
+
+        public List<SelectListItem> ProcessFromValue { get; set; }
+
+        public List<SelectListItem> ProcessToValue { get; set; }
+
+        public List<SelectListItem> SubStatus { get; set; }
+
+        public List<SelectListItem> SubStatusFromValue { get; set; }
+
+        public List<SelectListItem> SubStatusToValue { get; set; }
+        
+        public int Sequence { get; set; }        
+    }
+
+    public class BRActionModel
+    {
+        public BRActionModel()
+        {
+        }
+
+        public int Id { get; set; }
+
+        public int RuleId { get; set; }
+
+        // Send Email
+        public int ActionTypeId { get; set; }
+
+        public int Sequence { get; set; }
+
+        public List<SelectListItem> EMailTemplates { get; set; }
+
+        public List<SelectListItem> EMailGroups { get; set; }
+    }
+}
