3007970be2b9f2dfac87c8b696d2437345d9d2a0
dh_catharina_gunnarsson <cg@dhsolutions.se>
2016-09-29 09:26:32 +0200

    [FIX] Changed fieldlength LDAPFilter tblSettings

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -8,6 +8,10 @@ if not exists (select * from syscolumns inner join sysobjects on sysobjects.id =
 	ALTER TABLE tblCaseHistory ADD ActionExternalTime Int not NULL Default(0)
 Go
 
+-- tblSettings 
+ALTER TABLE tblSettings ALTER COLUMN LDAPFilter nvarchar(150)
+GO
+
 -- Last Line to update database version
 UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.27'
 
