a2c1a17adbe5abe8e8865c533e762e6bfc941d51
Vladimir <v.chechukha@gmail.com>
2016-11-14 15:56:22 +0300

    [Fix] #55914 add dependence units from selected department on create order page

---
+++ dhHelpdeskNG/dhHelpdesk_NG.Service/BusinessLogic/Mappers/Shared/Data/UnionItemDependentOverview.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DH.Helpdesk.Services.BusinessLogic.Mappers.Shared.Data
+{
+	internal class UnionItemDependentOverview : UnionItemOverview
+	{
+		public int? DependentId { get; set; }
+	}
+}
---
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/Orders/order.edit.js
@@ -0,0 +1,27 @@
+﻿function OrderEdit(parameters) {
+
+    $("#Orderer_DepartmentId").on("change",function() {
+                departmentEdit(parameters.Units, $(this).val());
+            });
+
+    function departmentEdit(units, depId) {
+        "use strict";
+
+        var unitSelect = $('#orderer_unit_select');
+        unitSelect.empty();
+        var availableUnits = $.grep(units,
+            function(unit) {
+                return (unit.DependentId === depId);
+            });
+        unitSelect.prepend("<option value='' selected='selected'></option>");
+        $.each(availableUnits,
+            function(index, unit) {
+                unitSelect.append($('<option/>',
+                {
+                    value: unit.Value,
+                    text: unit.Name
+                }));
+            });
+    }
+}
+
