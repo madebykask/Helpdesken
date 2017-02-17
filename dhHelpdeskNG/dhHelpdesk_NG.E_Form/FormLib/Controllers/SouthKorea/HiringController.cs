6be8e3950340356e1689028c444ad0b1b1f03693
dh_therese_andreasson <therese.andreasson@dhsolutions.se>
2017-02-17 11:32:02 +0100

    [UPD] KR - Add IsCaseDataChanged to Edit

--- dhHelpdeskNG/dhHelpdesk_NG.E_Form/FormLib/Controllers/SouthKorea/HiringController.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.E_Form/FormLib/Controllers/SouthKorea/HiringController.cs
@@ -191,6 +191,9 @@ namespace DH.Helpdesk.EForm.FormLib.Areas.SouthKorea.Controllers
                 ViewBag.CurrentCaseId = contract.Id;
             }
 
+            //Communicate with HD case that information is changed
+            FormLibSessions.IsCaseDataChanged = true;
+
             var model = FormModelFactory.InitEdit(xmlPath, contract, !string.IsNullOrEmpty(Request.QueryString["locked"]));
             model.Form = _contractRepository.GetFormByGuid(model.FormGuid);
             PopulateWithOptions(ref model);
