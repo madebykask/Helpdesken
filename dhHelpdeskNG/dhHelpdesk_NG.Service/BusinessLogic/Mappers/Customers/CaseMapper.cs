73869c7a4c59bf30c677ebfb2dcd50c882d33628
alexander nagornov <nagornovalex@gmail.com>
2017-03-29 12:19:36 +0300

    [FIX] Fixed Cases summary ef query.

--- dhHelpdeskNG/dhHelpdesk_NG.Service/BusinessLogic/Mappers/Customers/CaseMapper.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Service/BusinessLogic/Mappers/Customers/CaseMapper.cs
@@ -11,29 +11,30 @@
     public static class CaseMapper
     {
         public static CustomerCases[] MapToCustomerCases(
-                                            this IQueryable<Customer> query,
-                                            IQueryable<CaseFieldSetting> caseFieldSettings,
-                                            IQueryable<Problem> problems,
-                                            int userId)
+                                           this IQueryable<Customer> customers,
+                                           IQueryable<CaseFieldSetting> caseFieldSettings,
+                                           IQueryable<Problem> problems, //not used
+                                           int userId)
         {
-            var entities = query.Select(cus => new
-                                            {
-                                                CustomerId = cus.Id,
-                                                CustomerName = cus.Name,
-                                                CasesInProgress = cus.Cases.Where(c => c.FinishingDate == null && c.Deleted == 0).Count(),
-                                                CasesUnreaded = cus.Cases.Where(c => c.Unread == 1 && c.Deleted == 0 && c.FinishingDate == null).Count(),
-                                                CasesInRest = cus.Cases.Where(c => c.FinishingDate == null && c.StateSecondary_Id != null && c.StateSecondary.IncludeInCaseStatistics == 0 && c.Deleted == 0).Count(),
-                                                CasesMy = cus.Cases.Where(c => c.FinishingDate == null &&
-                                                                          c.Deleted == 0 &&
-                                                                          (c.Performer_User_Id == userId ||
-                                                                          (caseFieldSettings.Where(cf => cf.Customer_Id == cus.Id &&
-                                                                                                        cf.Name == GlobalEnums.TranslationCaseFields.CaseResponsibleUser_Id.ToString() &&
-                                                                                                        cf.ShowOnStartPage == 1).FirstOrDefault() == null ? false :
-                                                                                                        c.CaseResponsibleUser_Id == userId
-                                                                          ))
-                                                                         ).Count()
+            var entities = customers.Select(customer => new
+            {
+                CustomerId = customer.Id,
+                CustomerName = customer.Name,
+                CasesInProgress = customer.Cases.Where(c => c.FinishingDate == null && c.Deleted == 0).Count(),
+                CasesUnreaded = customer.Cases.Where(c => c.Unread == 1 && c.Deleted == 0 && c.FinishingDate == null).Count(),
+                CasesInRest = customer.Cases.Where(c => c.FinishingDate == null && c.StateSecondary_Id != null && c.StateSecondary.IncludeInCaseStatistics == 0 && c.Deleted == 0).Count(),
 
-                                                }).ToArray();
+                CasesMy =
+                                                      customer.Cases.Where(c => c.FinishingDate == null &&
+                                                                                c.Deleted == 0 &&
+                                                                                (c.Performer_User_Id == userId ||
+                                                                                  (c.CaseResponsibleUser_Id == userId &&
+                                                                                   caseFieldSettings.Any(cf => cf.Customer_Id == c.Customer_Id &&
+                                                                                                          cf.Name == GlobalEnums.TranslationCaseFields.CaseResponsibleUser_Id.ToString() &&
+                                                                                                          cf.ShowOnStartPage == 1)))).Count()
+
+
+            }).ToArray();
 
             var overviews = entities.Select(c => new CustomerCases(
                                                 c.CustomerId,
