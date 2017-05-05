ec1247589165c4158e5da0a261cae2486979575a
Vladimir <v.chechukha@gmail.com>
2017-05-05 14:52:07 +0300

    [ADD] #57647 Self-service plupload translations

--- dhHelpdeskNG/dhHelpdesk_NG.Web_SelfService/Content/js/dhHelpdesk.js
+++ dhHelpdeskNG/dhHelpdesk_NG.Web_SelfService/Content/js/dhHelpdesk.js
@@ -21,6 +21,39 @@ function getBreadcrumbs(a) {
     return path;
 }
 
+
+function PluploadTranslation(languageId) {
+    if (languageId == 1) {
+        plupload.addI18n({
+            'Select files': 'Välj filer',
+            'Add files to the upload queue and click the start button.': 'Lägg till filer till kön och tryck på start.',
+            'Filename': 'Filnamn',
+            'Status': 'Status',
+            'Size': 'Storlek',
+            'Add files': 'Lägg till filer',
+            'Add Files': 'Ladda upp',
+            'Start Upload': 'Lägg till',
+            'Stop current upload': 'Stoppa uppladdningen',
+            'Start uploading queue': 'Starta uppladdningen',
+            'Drag files here.': 'Dra filer hit'
+        });
+    }
+
+    if (languageId == 2) {
+        plupload.addI18n({
+            'Select files': 'Select files',
+            'Add files to the upload queue and click the start button.': 'Add files to the upload queue and click the start button.',
+            'Filename': 'Filename',
+            'Status': 'Status',
+            'Size': 'Size',
+            'Add files': 'Add files',
+            'Stop current upload': 'Stop current upload',
+            'Start uploading queue': 'Start uploading queue',
+            'Drag files here.': 'Drag files here.'
+        });
+    }
+}
+
 function today() {
     var today = new Date();
     var dd = today.getDate();
