2596e6e23362832c069affc19fdb721dc53a13aa
Vladimir <v.chechukha@gmail.com>
2017-05-24 16:47:29 +0300

    [ADD] #57781 Questionnaire statistics show comments

--- Database/SQLScripts/Translation.sql
+++ Database/SQLScripts/Translation.sql
@@ -5837,6 +5837,13 @@ begin
 end
 GO
 
+If not exists (select * from tbltext where id = 1814)
+	insert into tbltext (id, TextString) VALUES (1814, 'Kommentarer')
+GO
+If not exists (select * from tblTextTranslation where text_id = 1814 and Language_Id = 2)
+	insert into tblTextTranslation(Text_Id, Language_Id, TextTranslation) VALUES(1814, 2, 'Comments')
+GO
+
 
 -- *** Run this last when put translation script above this line **--
 update tblTextTranslation set CreatedDate = GETDATE(), ChangedDate  = GETDATE() where CreatedDate is null
---
+++ dhHelpdeskNG/dhHelpdesk_NG.Service/Response/Questionnaire/OptionNote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DH.Helpdesk.Services.Response.Questionnaire
+{
+    public class OptionNote
+    {
+        public string NoteText { get; set; }
+
+        public decimal CaseId { get; set; }
+    }
+}
