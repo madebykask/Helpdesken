b4923bb675c2c6deee922d903c206d15df8cb23a
Kristian Cederberg <kc@dhsolutions.se>
2017-06-02 12:21:39 +0200

    [UPDATE] SQLScripts

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -5,4 +5,6 @@
 -- Last Line to update database version
 UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.33'
 
-
+--UPDATE tblCaseSolutionCondition field length
+ALTER TABLE tblCaseSolutionCondition
+ALTER COLUMN [Values] nvarchar(4000)
