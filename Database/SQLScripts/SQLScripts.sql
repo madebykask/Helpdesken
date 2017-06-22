57a093a1d942df87b23c6f0c783e1c8f3915f348
dh_catharina_gunnarsson <cg@dhsolutions.se>
2017-06-22 13:33:48 +0200

    [ADD] #58173 New field in tblSettings - LDAPCreateOrganization

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -4,6 +4,12 @@ if not exists (select * from syscolumns inner join sysobjects on sysobjects.id =
 	ALTER TABLE [dbo].[tblQuestionnaire] ADD [ExcludeAdministrators] bit not null DEFAULT(0)
 GO
 
+-- New field in tblSettings
+if not exists (select * from syscolumns inner join sysobjects on sysobjects.id = syscolumns.id where syscolumns.name = N'LDAPCreateOrganization' and sysobjects.name = N'tblSettings')
+   ALTER TABLE tblSettings ADD LDAPCreateOrganization int NOT NULL Default(0)
+GO
+
+
 -- Last Line to update database version
 UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.33'
 
