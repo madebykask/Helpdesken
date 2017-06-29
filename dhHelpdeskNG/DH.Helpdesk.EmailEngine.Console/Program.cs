ea3fc7f53b70dbcff5da73960802c58a0a0a616d
dh_johan_weinitz <johan.weinitz@dhsolutions.se>
2017-06-29 11:24:10 +0200

    #59769 Tested and refactored EmailEngine

---
+++ dhHelpdeskNG/DH.Helpdesk.EmailEngine.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using DH.Helpdesk.EmailEngine.Library;
+using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DH.Helpdesk.EmailEngine.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XmlConfigurator.Configure();
+            var logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            var emailProcessor = new EmailProcessor(logger);
+
+            emailProcessor.ProcessEmails();
+        }
+    }
+}
