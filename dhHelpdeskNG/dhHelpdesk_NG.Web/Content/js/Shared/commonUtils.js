b404f3c86c4b38294c083ffffc28338bb4cde17c
nagornov alex <NagornovAlex@gmail.com>
2017-08-22 18:17:30 +0300

    [FIX] #58595 Removed debug logs.

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/Shared/commonUtils.js
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/Shared/commonUtils.js
@@ -5,13 +5,11 @@ CommonUtils.createDelayFunc = function () {
     var timer = 0;
     return function (callback, delay) {
         if (timer) {
-            console.log('cancelling request!!!');
             clearTimeout(timer);
         }
 
         timer = window.setTimeout(function () {
             timer = null;
-            console.log('now request is running!!!');
             callback();
         }, delay);
     }
