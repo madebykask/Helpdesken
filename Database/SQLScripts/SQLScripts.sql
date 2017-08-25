6766fbcf2a1a1a9e73605a4406ea8ebae53749c2
dh_therese_andreasson <therese.andreasson@dhsolutions.se>
2017-08-25 16:30:21 +0200

    [ADD] Add new ExtendedCase table to script

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -1193,5 +1193,21 @@ GO
 ALTER INDEX ALL ON tblCase REBUILD
 GO
 
+
+if not exists(select * from sysobjects WHERE Name = N'ExtendedCaseFormState')
+BEGIN
+
+	CREATE TABLE [dbo].[ExtendedCaseFormState](
+	[Id] [int]  NOT NULL PRIMARY KEY IDENTITY(1,1),
+	[ExtendedCaseDataId] [int] NOT NULL,
+	[TabId] [nvarchar](50) NOT NULL,
+	[SectionId] [nvarchar](50) NOT NULL,
+	[SectionIndex] [int] NOT NULL,
+	[Key] [nvarchar](50) NOT NULL,
+	[Value] [nvarchar](50) NOT NULL,
+	CONSTRAINT [FK_ExtendedCaseFormState_ExtendedCaseData] FOREIGN KEY([ExtendedCaseDataId]) REFERENCES [dbo].[ExtendedCaseData] ([Id]))
+
+END
+
 -- Last Line to update database version
 UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.33'
