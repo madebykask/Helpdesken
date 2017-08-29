aa5dd575f0ce0e8e4b6cd3f0074c2b1fc3806a9b
Vladimir <v.chechukha@gmail.com>
2017-08-29 16:14:17 +0300

    [UPD] #58179 Prod area case type translation

--- Database/SQLScripts/Translation.sql
+++ Database/SQLScripts/Translation.sql
@@ -6165,5 +6165,13 @@ GO
 If not exists (select * from tblTextTranslation where text_id = 1851 and Language_Id = 2)
 	insert into tblTextTranslation(Text_Id, Language_Id, TextTranslation) VALUES(1851, 2, 'Show fields in subject')
 GO
+
+If not exists (select * from tbltext where id = 1852)
+	insert into tbltext (id, TextString) VALUES (1852, 'Visa endast för')
+GO
+If not exists (select * from tblTextTranslation where text_id = 1852 and Language_Id = 2)
+	insert into tblTextTranslation(Text_Id, Language_Id, TextTranslation) VALUES(1852, 2, 'Only show for')
+GO
+
 -- *** Run this last when put translation script above this line **--
 update tblTextTranslation set CreatedDate = GETDATE(), ChangedDate  = GETDATE() where CreatedDate is null
