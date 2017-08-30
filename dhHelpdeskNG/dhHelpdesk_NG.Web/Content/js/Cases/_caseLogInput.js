7b4a9e005fa6b6d2762582f0815600e97e2afe61
dh_catharina_gunnarsson <cg@dhsolutions.se>
2017-08-30 09:21:16 +0200

    [FIX] #57966 Inform initiator tick box - conflicting rules

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/Cases/_caseLogInput.js
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/Cases/_caseLogInput.js
@@ -57,23 +57,14 @@ function LogInitForm() {
         $('#case__StateSecondary_Id option[value=' + curVal + ']').attr('selected', 'selected');
         $.post('/Cases/ChangeStateSecondary', { 'id': $(this).val() }, function (data) {
             // disable send mail checkbox
-            if (data.CommunicateWithNotifier == 1 && data.NoMailToNotifier == 0) {
-                $('#CaseLog_SendMailAboutCaseToNotifier').prop('checked', true);
-                $('#CaseLog_SendMailAboutCaseToNotifier').attr('disabled', false);
-            } else{
-
+            if (data.NoMailToNotifier == 1) {
                 $('#CaseLog_SendMailAboutCaseToNotifier').prop('checked', false);
-                //$('#CaseLog_SendMailAboutCaseToNotifier').attr('disabled', true);
+                $('#CaseLog_SendMailAboutCaseToNotifier').attr('disabled', true);
             }
-
-            //if (data.NoMailToNotifier == 1) {
-            //    $('#CaseLog_SendMailAboutCaseToNotifier').prop('checked', false);
-            //    $('#CaseLog_SendMailAboutCaseToNotifier').attr('disabled', true);
-            //}
-            //else {
-            //    $('#CaseLog_SendMailAboutCaseToNotifier').prop('checked', true);
-            //    $('#CaseLog_SendMailAboutCaseToNotifier').attr('disabled', false);
-            //}
+            else {
+                $('#CaseLog_SendMailAboutCaseToNotifier').prop('checked', false);
+                $('#CaseLog_SendMailAboutCaseToNotifier').attr('disabled', false);
+            }
             // set workinggroup id
             var exists = $('#case__WorkingGroup_Id option[value=' + data.WorkingGroup_Id + ']').length;
             if (exists > 0 && data.WorkingGroup_Id > 0) {
--- dhHelpdeskNG/dhHelpdesk_NG.Web/Controllers/CasesController.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Controllers/CasesController.cs
@@ -2011,7 +2011,6 @@ namespace DH.Helpdesk.Web.Controllers
             int workinggroupId = 0;
             int noMailToNotifier = 0;
             int reCalculateWatchDate = 0;
-            int communicateWithNotifier = 0;
 
             if (id.HasValue)
             {
@@ -2019,16 +2018,12 @@ namespace DH.Helpdesk.Web.Controllers
                 reCalculateWatchDate = s != null ? s.RecalculateWatchDate : 0;
                 noMailToNotifier = s != null ? s.NoMailToNotifier : 0;
                 workinggroupId = s != null ? s.WorkingGroup_Id.HasValue ? s.WorkingGroup_Id.Value : 0 : 0;
-
-                var customer = _customerService.GetCustomer(SessionFacade.CurrentCustomer.Id);
-                communicateWithNotifier = customer != null ? customer.CommunicateWithNotifier : 0;
             }
             return Json(new
             {
                 NoMailToNotifier = noMailToNotifier,
                 WorkingGroup_Id = workinggroupId,
-                ReCalculateWatchDate = reCalculateWatchDate,
-                CommunicateWithNotifier = communicateWithNotifier
+                ReCalculateWatchDate = reCalculateWatchDate
             });
         }
 
