9af7e2b43b9f8cf91481d02d805218b2624e2cd8
dh_catharina_gunnarsson <cg@dhsolutions.se>
2017-09-20 08:00:06 +0200

    [UPD] Updated SQLScripts

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -1,7 +1,14 @@
 ﻿
 --update DB from 5.3.33 to 5.3.34 version
 
-	
+--UPDATE tblCustomerUser field length
+ALTER TABLE tblCustomerUser
+ALTER COLUMN CaseDepartmentFilter nvarchar(100)
+
+-- New field in tblCaseSolutionConditionProperties
+if not exists (select * from syscolumns inner join sysobjects on sysobjects.id = syscolumns.id where syscolumns.name = N'TableFieldStatus' and sysobjects.name = N'tblCaseSolutionConditionProperties')
+   ALTER TABLE tblCaseSolutionConditionProperties ADD TableFieldStatus nvarchar(100) NULL
+GO	
 
 
 -- Last Line to update database version
