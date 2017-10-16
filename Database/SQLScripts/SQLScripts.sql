1cddd51218ba3f0db2163ed8571a645776412b15
nagornov alex <NagornovAlex@gmail.com>
2017-10-16 17:34:43 +0300

    [FIX] tblMetaData.MetaDataText updated to be nvarchar(max). Required for AM.

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -147,6 +147,9 @@ begin
 	ALTER TABLE [dbo].[tblCaseDocumentTemplate] DROP COLUMN [PageNumbersUse] 
 end
 
+-- set MetaDataText to max size (AM issue)
+ALTER TABLE [dbo].[tblMetaData]
+ALTER COLUMN MetaDataText nvarchar(MAX) NOT NULL
 
 GO
 -- Last Line to update database version
