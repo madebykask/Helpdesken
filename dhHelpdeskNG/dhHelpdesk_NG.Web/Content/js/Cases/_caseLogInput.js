1af173c01540cbafd1cd196737b8bafbdb8efd6b
Vladimir <v.chechukha@gmail.com>
2017-11-24 17:22:56 +0300

    [FIX] #59576 Invoice validation

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/Cases/_caseLogInput.js
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/Cases/_caseLogInput.js
@@ -5,84 +5,12 @@
 */
 function LogInitForm() {
 
-    var utils = {
-        okText: '',
-        cancelText: '',
-        yesText: '',
-        noText: '',
-
-        init: function (okText, cancelText, yesText, noText) {
-            utils.okText = okText;
-            utils.cancelText = cancelText;
-            utils.yesText = yesText;
-            utils.noText = noText;
-        },
-
-        showMessage: function (message, type) {
-            $().toastmessage('showToast', {
-                text: utils.replaceAll(message, '|', '<br />'),
-                sticky: false,
-                position: 'top-center',
-                type: type || 'notice',
-                closeText: '',
-                stayTime: 10000,
-                inEffectDuration: 1000,
-                width: 700
-            });
-        },
-
-        showWarning: function (message) {
-            utils.showMessage(message, 'warning');
-        },
-
-        showError: function (message) {
-            utils.showMessage(message, 'error');
-        },
-
-        replaceAll: function (string, omit, place, prevstring) {
-            if (prevstring && string === prevstring)
-                return string;
-            prevstring = string.replace(omit, place);
-            return utils.replaceAll(prevstring, omit, place, string);
-        }
-    }
-
-    var $form = $('#target');
     var $finishTypeContainer = $('#divFinishingType');
     var $finishTypeId = $("#CaseLog_FinishingType");
     var $finishTypeBreadcrubs = $("#divBreadcrumbs_FinishingType");
     var $finishDate = $('#CaseLog_FinishingDate');
     var EDIT_LOG_URL = '/Cases/EditLog';
-
-    var getValidationErrorMessage = function (extraMessage) {
-        var validationMessages = window.parameters.validationMessages || '';
-        var requiredFieldsMessage = window.parameters.requiredFieldsMessage || '';
-        var mandatoryFieldsText = window.parameters.mandatoryFieldsText || '';
-        var messages = [requiredFieldsMessage, '<br />', mandatoryFieldsText, ':'];
-        $("label.error").each(function (key, el) {
-            if ($(el).css('display') === 'none') {
-                return true;
-            }
-            var errorText = $(el).text();
-            $.each(validationMessages, function (index, validationMessage) {
-                errorText = '<br />' + '[' + utils.replaceAll(errorText, validationMessage, '').trim() + ']';
-            });
-            messages.push(errorText);
-        });
-
-        messages.push(extraMessage);
-
-        return messages.join('');
-    };
-
-    var isFormValid = function () {
-        if (!$form.valid()) {
-            utils.showError(getValidationErrorMessage());
-            return false;
-        }
-        return true;
-    };
-
+    
     $finishTypeContainer.find('ul.dropdown-menu li a').click(function (e) {
         e.preventDefault();
         var value = $(this).attr('value');
@@ -115,11 +43,7 @@ function LogInitForm() {
     $("#btnCaseCharge").on('click', function (ev) {
         window.caseChargeObj.show();
     });
-
-    $('#log-action-save').on('click',function(e) {
-        isFormValid();
-    });
-
+    
     $('#case__StateSecondary_Id').change(function () {
         $('#CaseLog_SendMailAboutCaseToNotifier').removeAttr('disabled');
         var curVal = $('#case__StateSecondary_Id').val();
