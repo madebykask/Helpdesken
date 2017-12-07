1b5639d04e51a3526337468ed278165ca6fc3598
dh_johan_weinitz <johan.weinitz@dhsolutions.se>
2017-12-07 16:49:56 +0100

    [FIX] Changed back, rather have duplicate code than risk failing

--- utils/GenerateAssemblyInfo.bat
+++ utils/GenerateAssemblyInfo.bat
@@ -1,2 +1,7 @@
-GenerateAssemblyInfoNoPause.bat
+:: Shurtcut to run MSBuild.exe with GenerateAsseblyInfo.xml 
+::	Run GenerateAssemblyInfo.bat [FILE_NAME] 
+:: where FILE_NAME is where to put generated content for AssemblyInfo.cs
+:: HINT: for more verbose MSBuild output add /v:d parameter
+echo =========== resolving version from GIT ======================
+%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe "%~dp0\GenerateAssemblyInfo.xml" /p:AssemblyFileName=%1
 pause
