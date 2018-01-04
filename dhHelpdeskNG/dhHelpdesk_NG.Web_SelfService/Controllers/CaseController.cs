fd468d4b453749abacff736293a81a1bfdf42408
Vladimir <v.chechukha@gmail.com>
2018-01-04 18:11:20 +0300

    [FIX] #59993 Format incorrect when showing HTML-tag in SelfService comments

--- dhHelpdeskNG/dhHelpdesk_NG.Web_SelfService/Controllers/CaseController.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web_SelfService/Controllers/CaseController.cs
@@ -1070,6 +1070,7 @@ namespace DH.Helpdesk.SelfService.Controllers
         }
 
         [HttpGet]
+        [ValidateInput(false)]
         public ActionResult _CaseLogNote(int caseId, string note, string logFileGuid)
         {
             SaveExternalMessage(caseId, note, logFileGuid);            
