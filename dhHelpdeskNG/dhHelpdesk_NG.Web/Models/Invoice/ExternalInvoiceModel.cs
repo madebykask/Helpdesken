d1a42e8a152993467f7f3c86b219b71bd344d784
nagornovalex <NagornovAlex@gmail.com>
2018-04-15 20:25:02 +0300

    [FIX]#60804 Invoices not added

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Models/Invoice/ExternalInvoiceModel.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Models/Invoice/ExternalInvoiceModel.cs
@@ -1,11 +1,32 @@
-﻿namespace DH.Helpdesk.Web.Models.Invoice
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace DH.Helpdesk.Web.Models.Invoice
 {
-	public class ExternalInvoiceModel
+    public class ExternalInvoiceModel
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public decimal Amount { get; set; }
-		public bool Charge { get; set; }
+        
+        public decimal Amount { get; set; }
+
+	    public string AmountValue
+	    {
+	        get { return Amount.ToString("#.##"); }
+	        set
+	        {
+	            var culture = (CultureInfo)Thread.CurrentThread.CurrentCulture.Clone();
+	            culture.NumberFormat.NumberDecimalSeparator = culture.NumberFormat.CurrencyDecimalSeparator = culture.NumberFormat.PercentDecimalSeparator = ".";
+	            culture.NumberFormat.NumberGroupSeparator = culture.NumberFormat.CurrencyGroupSeparator = culture.NumberFormat.PercentGroupSeparator = " ";
+	            Amount = 
+                    decimal.Parse(value, 
+                        NumberStyles.AllowLeadingWhite | NumberStyles.AllowTrailingWhite | 
+                        NumberStyles.AllowDecimalPoint | NumberStyles.AllowThousands, culture);
+	        }
+	    }
+
+        public bool Charge { get; set; }
 
 		public InvoiceRowViewModel InvoiceRow { get; set; }
 	}
