7d786e6ec3da1c734ee13668c246738b9455ffaf
nagornovalex <NagornovAlex@gmail.com>
2018-05-23 12:00:42 +0300

    [FIX]#61930 - GDPR - Time Executed.

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Areas/Admin/Controllers/GlobalSettingController.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Areas/Admin/Controllers/GlobalSettingController.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Globalization;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -1584,6 +1585,8 @@ namespace DH.Helpdesk.Web.Areas.Admin.Controllers
             var caseText = Translation.GetCoreTextTranslation("Ärende");
             var logPostsText = Translation.GetCoreTextTranslation("Ärendelogg");
 
+            var userTimeZone = TimeZoneInfo.FindSystemTimeZoneById(SessionFacade.CurrentUser.TimeZoneId);
+
             var model = new List<GdprOperationsHistoryListItem>();
 
             //replace field names with field labels
@@ -1604,7 +1607,7 @@ namespace DH.Helpdesk.Web.Areas.Admin.Controllers
                     Cases = item.ClosedOnly ? closedText : $"{closedText}, {openedText}",
                     Data = formattedFields.Any() ? string.Join(", ", formattedFields) : "",
                     AttachedFiles = attachedFilesFormatted.ToString().Trim(',').Trim(),
-                    Executed = item.ExecutedDate.ToString("yyyy-MM-dd HH:mm:ss")
+                    Executed = TimeZoneInfo.ConvertTimeFromUtc(item.ExecutedDate, userTimeZone).ToString("yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)
                 };
 
                 model.Add(modelItem);
