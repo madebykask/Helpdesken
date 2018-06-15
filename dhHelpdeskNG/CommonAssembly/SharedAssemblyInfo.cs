c4e04e5a341e5aa138985b3ad367c4f74243f4b8
Mikhail Romanyuk <romanyuk_m@outlook.com>
2018-06-15 17:34:29 +0300

    [UPD] Updated Version generation script

---
+++ dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("DH Solutions")]
+[assembly: AssemblyCopyright("© 2015 DH Solutions")]
+[assembly: AssemblyVersion("5.3.37.165")]
+[assembly: AssemblyFileVersion("5.3.37.165")]
+[assembly: AssemblyInformationalVersion("5.3.37.165-459f0ba84")]
+
+namespace DH.Helpdesk
+{
+    public static class Version
+    {
+        public const string FULL_VERSION = "5.3.37.165-459f0ba84";
+    }
+}
