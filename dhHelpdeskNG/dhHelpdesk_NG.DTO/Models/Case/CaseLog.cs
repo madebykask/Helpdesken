ace74a785ace9eeeaa784ce57ec708359dd22fc0
nagornovalex <NagornovAlex@gmail.com>
2018-07-28 18:57:08 +0300

    [UPD] #62182  Save original MessageID for identification.

--- dhHelpdeskNG/dhHelpdesk_NG.DTO/Models/Case/CaseLog.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.DTO/Models/Case/CaseLog.cs
@@ -44,49 +44,49 @@ namespace DH.Helpdesk.BusinessData.Models.Case
         public int? UserId { get; set; }
         public string UserName { get; set; }
         public int WorkingTime { get; set; }
-		public int Overtime { get; set; }
-		public bool HighPriority { get; set; }
+        public int Overtime { get; set; }
+        public bool HighPriority { get; set; }
 
-		//TODO: remove when this business model will not be used as viewmodel
-	    public int WorkingTimeHour
-	    {
-		    get { return CalculateWorkingTimeHour(WorkingTime); }
-		    set { WorkingTime = value * 60 + WorkingTimeMinute; }
-	    }
+        //TODO: remove when this business model will not be used as viewmodel
+        public int WorkingTimeHour
+        {
+            get { return CalculateWorkingTimeHour(WorkingTime); }
+            set { WorkingTime = value * 60 + WorkingTimeMinute; }
+        }
 
-	    public int WorkingTimeMinute
-	    {
-		    get { return CalculateWorkingTimeMinute(WorkingTime); }
-		    set { WorkingTime = value + WorkingTimeHour * 60; }
-		}
+        public int WorkingTimeMinute
+        {
+            get { return CalculateWorkingTimeMinute(WorkingTime); }
+            set { WorkingTime = value + WorkingTimeHour * 60; }
+        }
 
-	    public int OvertimeHour
-	    {
-		    get { return CalculateWorkingTimeHour(Overtime); }
-		    set { Overtime = value * 60 + OvertimeMinute; }
-		}
+        public int OvertimeHour
+        {
+            get { return CalculateWorkingTimeHour(Overtime); }
+            set { Overtime = value * 60 + OvertimeMinute; }
+        }
 
-	    public int OvertimeMinute
-	    {
-		    get { return CalculateWorkingTimeMinute(Overtime); }
-		    set { Overtime = value + OvertimeHour * 60; }
-		}
+        public int OvertimeMinute
+        {
+            get { return CalculateWorkingTimeMinute(Overtime); }
+            set { Overtime = value + OvertimeHour * 60; }
+        }
 
-	    private int CalculateWorkingTimeHour(int workingTime)
-		{
-			return workingTime >= 60 ? (int)Math.Round((double)(workingTime / 60), 0) : 0;
-		}
+        private int CalculateWorkingTimeHour(int workingTime)
+        {
+            return workingTime >= 60 ? (int)Math.Round((double)(workingTime / 60), 0) : 0;
+        }
 
-		private int CalculateWorkingTimeMinute(int workingTime)
-		{
-			return workingTime >= 60 ? (int)workingTime % 60 : workingTime;
-		}
+        private int CalculateWorkingTimeMinute(int workingTime)
+        {
+            return workingTime >= 60 ? (int)workingTime % 60 : workingTime;
+        }
 
-		/// <summary>
-		/// checkbox value indicates send log to parent/child cases
-		/// </summary>
-		public bool? SendLogToParentChildLog { get; set; }
-		public InvoiceRow InvoiceRow { get; set; }
+        /// <summary>
+        /// checkbox value indicates send log to parent/child cases
+        /// </summary>
+        public bool? SendLogToParentChildLog { get; set; }
+        public InvoiceRow InvoiceRow { get; set; }
 
         public int? OldLog_Id { get; set; }
     }
