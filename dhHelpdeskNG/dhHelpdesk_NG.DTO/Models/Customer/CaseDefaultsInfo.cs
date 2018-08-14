5fc123163d33f62c013f3bfdf09dd5f2ffac1312
nagornovalex <NagornovAlex@gmail.com>
2018-08-14 09:31:41 +0300

    [FIX] #63291 - Possibility to move case to customer without user rights - Case type;

--- dhHelpdeskNG/dhHelpdesk_NG.DTO/Models/Customer/CaseDefaultsInfo.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.DTO/Models/Customer/CaseDefaultsInfo.cs
@@ -4,6 +4,7 @@
     {
         public int? RegionId { get; set; }
         public int CaseTypeId { get; set; }
+        public int EmailCaseTypeId { get; set; }
         public int? SupplierId { get; set; }
         public int? PriorityId { get; set; }
         public int? StatusId { get; set; }
