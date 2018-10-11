6ba7caf5dd16a2ad970d22fa54adb464b3f6e5da
nagornovalex <NagornovAlex@gmail.com>
2018-10-11 10:41:12 +0300

    [FIX]#64678 Add initiator from case registration - Hotfix;

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Infrastructure/Extensions/HtmlHelperExtensions/Content/DropDownContent.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Infrastructure/Extensions/HtmlHelperExtensions/Content/DropDownContent.cs
@@ -1,28 +1,46 @@
-﻿namespace DH.Helpdesk.Web.Infrastructure.Extensions.HtmlHelperExtensions.Content
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace DH.Helpdesk.Web.Infrastructure.Extensions.HtmlHelperExtensions.Content
 {
     using System;
     using System.Collections.Generic;
 
     public sealed class DropDownContent
     {
-        public DropDownContent(List<DropDownItem> items)
+        #region ctor()
+
+        public DropDownContent(IList<SelectListItem> items)
         {
             if (items == null)
-            {
-                throw new ArgumentNullException("items", "Value cannot be null.");
-            }
+                throw new ArgumentNullException(nameof(items), "Value cannot be null.");
 
-            this.Items = items;
+            Items = items.Select(x => new DropDownItem(x.Text, x.Value)).ToList();
+            SelectedValue = items.FirstOrDefault(x => x.Selected)?.Value;
+        }
+
+        public DropDownContent(List<DropDownItem> items)
+            : this(items, null)
+        {
         }
 
         public DropDownContent(List<DropDownItem> items, string selectedValue)
-            : this(items)
         {
+            if (items == null)
+                throw new ArgumentNullException(nameof(items), "Value cannot be null.");
+
+            this.Items = items;
             this.SelectedValue = selectedValue;
         }
+
+        #endregion
+
+        #region Public Properties
 
         public List<DropDownItem> Items { get; private set; }
 
         public string SelectedValue { get; private set; }
+
+        #endregion
     }
 }
