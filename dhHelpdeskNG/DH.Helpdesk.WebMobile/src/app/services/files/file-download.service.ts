796fb03a95f2aede6dfa0bec6acc42a4004c6435
nagornovalex <NagornovAlex@gmail.com>
2018-11-01 20:55:37 +0300

    #65249 - Files - open in new tab. Openning file in the same window.

--- dhHelpdeskNG/DH.Helpdesk.WebMobile/src/app/services/files/file-download.service.ts
+++ dhHelpdeskNG/DH.Helpdesk.WebMobile/src/app/services/files/file-download.service.ts
@@ -14,33 +14,13 @@ export class FileDownloadService extends HttpApiServiceBase {
     }
 
     downloadCaseFile(caseId:number, fileId:number,  fileName:string) {
-        var newTab = this.windowWrapper.nativeWindow.open("about:blank", 'newtab');
+        let window = this.windowWrapper.nativeWindow;        
         let url = this.buildResourseUrl(`/api/case/${caseId}/file/${fileId}`, { inline: true }, true, false);
         this.getFileBody(url, null).pipe(
             take(1),
         ).subscribe(data => {
             //saveAs(data, fileName); // uses file-saver.js
-            //this.openFile(data);            
-            newTab.location.href = window.URL.createObjectURL(data);
+            window.location.href = window.URL.createObjectURL(data);
         });
     }
-
-    downloadCaseFileOld(caseId:number, fileId:number,  fileName:string) {
-        let url = this.buildResourseUrl(`/api/case/${caseId}/file/${fileId}`, { inline: true }, true, false);
-        this.getFileBody(url, null).pipe(
-            take(1),
-        ).subscribe(data => {
-            //saveAs(data, fileName); // uses file-saver.js
-            this.openFile(data);            
-        });
-    }
-
-    //alternative approach
-    openFile(blob){
-        var url = window.URL.createObjectURL(blob);
-        var pwa = window.open(url);
-        if (!pwa || pwa.closed || typeof pwa.closed == 'undefined') {
-            alert( 'Please disable your Pop-up blocker and try again.');
-        }
-    }  
 }
