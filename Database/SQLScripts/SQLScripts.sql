302a56880a2246bb5cef35501ed120815fd0613e
Johan Weinitz <johan.weinitz@dhsolutions.se>
2018-12-10 11:18:37 +0100

    #65926 La till index på tblCaseStatistics

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -1,6 +1,14 @@
 ﻿--update DB from 5.3.39 to 5.3.40 version
 
 
+
+-- Create index on tblCaseStatistics to tblCase
+IF NOT EXISTS(SELECT * FROM sys.indexes WHERE name = 'idx_casestatistics_case' AND object_id = OBJECT_ID('tblCaseStatistics'))
+BEGIN
+	CREATE NONCLUSTERED INDEX idx_casestatistics_case
+	ON [dbo].[tblCaseStatistics] ([Case_Id])
+	INCLUDE ([Id],[WasSolvedInTime])
+END
 -- Last Line to update database version
 UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.40'
 --ROLLBACK --TMP
