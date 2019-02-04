8c23086833d661314c75e9bcdd5f99ac86ef9320
Catharina Gunnarsson1 <cg@dhsolutions.se>
2019-02-04 13:52:52 +0100

    [ADD] #68675 Settings for show template on mobile (scripts)

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -1,5 +1,14 @@
 ﻿--update DB from 5.3.40 to 5.3.41 version
 
+if not exists (select * from syscolumns inner join sysobjects on sysobjects.id = syscolumns.id 
+               where syscolumns.name = N'ShowOnMobile' and sysobjects.name = N'tblCaseSolution')
+BEGIN
+    ALTER TABLE tblCaseSolution
+    ADD ShowOnMobile int NOT NULL DEFAULT(1)        
+END
+GO
+
+
 
 -- Last Line to update database version
 UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.41'
--- Database/SQLScripts/Translation.sql
+++ Database/SQLScripts/Translation.sql
@@ -7366,6 +7366,13 @@ If not exists (select * from tblTextTranslation where text_id = 2002 and Languag
 	insert into tblTextTranslation(Text_Id, Language_Id, TextTranslation) VALUES(2002, 2, 'Tabs')
 GO
 
+If not exists (select * from tbltext where id = 2003)
+	insert into tbltext (id, TextString) VALUES (2003, 'Visa på mobil')
+GO
+If not exists (select * from tblTextTranslation where text_id = 2003 and Language_Id = 2)
+	insert into tblTextTranslation(Text_Id, Language_Id, TextTranslation) VALUES(2003, 2, 'Show on mobile')
+GO
+
 
 -- *** Run this last when put translation script above this line *** --
 update tblTextTranslation set CreatedDate = GETDATE(), ChangedDate  = GETDATE() where CreatedDate is null
