d2367f76eb6308a87f7ebf636a1f141878a48083
Johan Weinitz <johan.weinitz@dhsolutions.se>
2019-02-05 10:00:55 +0100

    Hotfix for #68734 Problem with russian files names

--- dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
+++ dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
@@ -2,14 +2,14 @@
 
 [assembly: AssemblyCompany("DH Solutions")]
 [assembly: AssemblyCopyright("© 2015 DH Solutions")]
-[assembly: AssemblyVersion("5.3.40.279")]
-[assembly: AssemblyFileVersion("5.3.40.279")]
-[assembly: AssemblyInformationalVersion("5.3.40.279-3aeaf6163")]
+[assembly: AssemblyVersion("5.3.40.340")]
+[assembly: AssemblyFileVersion("5.3.40.340")]
+[assembly: AssemblyInformationalVersion("5.3.40.340-d81d4111a")]
 
 namespace DH.Helpdesk
 {
     public static class Version
     {
-        public const string FULL_VERSION = "5.3.40.279-3aeaf6163";
+        public const string FULL_VERSION = "5.3.40.340-d81d4111a";
     }
 }
