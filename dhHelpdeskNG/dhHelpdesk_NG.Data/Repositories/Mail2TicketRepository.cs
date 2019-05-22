6ee40b1b0fbfc78f43216b2cca032e5d95409e64
nagornovalex <NagornovAlex@gmail.com>
2019-05-22 15:05:47 +0300

    #70617 - Missing #14 for Mail2Ticket; Build error fix.

--- dhHelpdeskNG/dhHelpdesk_NG.Data/Repositories/Mail2TicketRepository.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Data/Repositories/Mail2TicketRepository.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DH.Helpdesk.Dal.Infrastructure;
 using DH.Helpdesk.Domain;
 using System.Linq;
@@ -6,15 +7,22 @@ namespace DH.Helpdesk.Dal.Repositories
 {
     public interface IMail2TicketRepository : IRepository<Mail2Ticket>
     {
+        IList<Mail2Ticket> GetCaseMail2Tickets(int caseId);
         void DeleteByCaseId(int id);
         void DeleteByLogId(int id);
     }
 
     public class Mail2TicketRepository : RepositoryBase<Mail2Ticket>, IMail2TicketRepository
     {
-        public Mail2TicketRepository(IDatabaseFactory databaseFactory) 
+        public Mail2TicketRepository(IDatabaseFactory databaseFactory)
             : base(databaseFactory)
         {
+        }
+
+        public IList<Mail2Ticket> GetCaseMail2Tickets(int caseId)
+        {
+            var items = Table.Where(m => m.Case_Id == caseId).ToList();
+            return items;
         }
 
         public void DeleteByCaseId(int id)
@@ -22,7 +30,7 @@ namespace DH.Helpdesk.Dal.Repositories
             var m2ts = Table.Where(m => m.Case_Id == id).ToList();
             if (m2ts.Any())
             {
-                DataContext.Mail2Tickets.RemoveRange(m2ts);               
+                DataContext.Mail2Tickets.RemoveRange(m2ts);
             }
         }
 
@@ -31,9 +39,9 @@ namespace DH.Helpdesk.Dal.Repositories
             var m2ts = Table.Where(m => m.Log_Id == id).ToList();
             if (m2ts.Any())
             {
-                DataContext.Mail2Tickets.RemoveRange(m2ts);                
+                DataContext.Mail2Tickets.RemoveRange(m2ts);
             }
         }
     }
-    
+
 }
