ecbed11e8bb52de6d8ae00c124aac0b3fd44d02d
nagornovalex <NagornovAlex@gmail.com>
2019-06-05 16:38:46 +0300

    71412 - Anwser to mail Assign case and change subject; M2T Test;

---
+++ dhHelpdeskNG/DH.Helpdesk.Tests/Mail2Ticket/Mail2TicketLogicTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace DH.Helpdesk.Tests.Mail2Ticket
+{
+    [TestFixture]
+    public class Mail2TicketLogicTests
+    {
+        [Test]
+        public void ExtractCaseNumberFromSubjectTest()
+        {
+            var subjectPatterns = new string[]
+            {
+                //"case:#;ärende:#",
+                //"case #;ärende #" // TODO: this will fail the test. need to find out if it was implemented for a reason or its a bug!
+            };
+
+            // 1. case number should not be found
+            var emailSubject = "RE: new case has 4 been created 7853";
+
+            foreach (var pattern in subjectPatterns)
+            {
+                var caseNumber = DH.Helpdesk.Mail2Ticket.Library.SharedFunctions.extractCaseNumberFromSubject(emailSubject, pattern);
+                Assert.AreEqual(caseNumber, 0); 
+            }
+
+            // 2. case number should be found
+            emailSubject = "RE: new case 7853, case:7853";
+
+            foreach (var pattern in subjectPatterns)
+            {
+                var caseNumber = DH.Helpdesk.Mail2Ticket.Library.SharedFunctions.extractCaseNumberFromSubject(emailSubject, pattern);
+                Assert.AreEqual(caseNumber, 7853);
+            }
+        }
+    }
+}
