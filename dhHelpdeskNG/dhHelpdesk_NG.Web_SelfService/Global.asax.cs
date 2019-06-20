46b9455f4e3eb1f155d1ad7d30bcbc4277d6bd15
Mikhail Romanyuk <romanyuk_m@outlook.com>
2019-06-20 11:48:38 +0300

    #71773 - Activate a case with extended case - error

--- dhHelpdeskNG/dhHelpdesk_NG.Web_SelfService/Global.asax.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web_SelfService/Global.asax.cs
@@ -39,6 +39,12 @@ namespace DH.Helpdesk.SelfService
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "ExtendedCase", // Route name
+                "Case/ExtendedCase/{caseId}", // URL with parameters
+                new {controller = "Case", action = "ExtendedCase"} // Parameter defaults
+            );
+
+            routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
                 new {controller = "Home", action = "Index", id = UrlParameter.Optional} // Parameter defaults
