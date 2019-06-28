6918acb4d76821d3a994dc2d92c1a788083eef31
nagornovalex <NagornovAlex@gmail.com>
2019-06-28 15:32:49 +0300

    #71951 - Anwser from mobile with M2T - whole mail in lognote; Sql script to update width to 512;

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -1,6 +1,12 @@
 ﻿--update DB from 5.3.42 to 5.3.43 version
 
-
+if exists (select * from syscolumns inner join sysobjects on sysobjects.id = syscolumns.id              
+		 where syscolumns.name = N'EMailAnswerSeparator' and sysobjects.name = N'tblSettings')
+BEGIN
+    ALTER TABLE tblSettings
+    ALTER COLUMN EMailAnswerSeparator nvarchar(512) not null    
+END
+GO
 
 -- Last Line to update database version
 UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.43'
