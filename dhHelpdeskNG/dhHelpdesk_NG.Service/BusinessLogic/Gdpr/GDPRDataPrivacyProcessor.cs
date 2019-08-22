d5ce442fdc0a64f86ca854b9027c4404e9634ef3
Johan Weinitz <johan.weinitz@dhsolutions.se>
2019-08-22 17:24:19 +0200

    [Fix] #73601, #73602 Check for logtype and used LL for deleting internal log files, removed internal lognote attachment option

--- dhHelpdeskNG/dhHelpdesk_NG.Service/BusinessLogic/Gdpr/GDPRDataPrivacyProcessor.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Service/BusinessLogic/Gdpr/GDPRDataPrivacyProcessor.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using DH.Helpdesk.BusinessData.Models.Gdpr;
 using DH.Helpdesk.BusinessData.OldComponents;
 using DH.Helpdesk.Common.Enums;
+using DH.Helpdesk.Common.Enums.Logs;
 using DH.Helpdesk.Common.Exceptions;
 using DH.Helpdesk.Common.Serializers;
 using DH.Helpdesk.Dal.Enums;
@@ -46,29 +47,32 @@ namespace DH.Helpdesk.Services.BusinessLogic.Gdpr
 
         private class CaseFileEntity
         {
-            #region ctor()
+			private LogFileType logType;
+			#region ctor()
 
-            public CaseFileEntity(int caseId, string caseNumber, string fileName)
-                : this(caseId, caseNumber, 0, fileName)
+			public CaseFileEntity(int caseId, string caseNumber, string fileName)
+                : this(caseId, caseNumber, 0, fileName, LogFileType.External)
             {
             }
 
-            public CaseFileEntity(int caseId, string caseNumber, int logId, string fileName)
-            {
-                CaseId = caseId;
-                CaseNumber = caseNumber;
-                LogId = logId;
-                FileName = fileName;
-            }
+			public CaseFileEntity(int caseId, string caseNumber, int logId, string fileName, LogFileType logType = LogFileType.External)
+			{
+				CaseId = caseId;
+				CaseNumber = caseNumber;
+				LogId = logId;
+				FileName = fileName;
+				this.logType = logType;
+			}
 
-            #endregion
+			#endregion
 
-            public int CaseId { get; private set; }
+			public int CaseId { get; private set; }
             public string CaseNumber { get; private set; }
             public int LogId { get; private set; }
             public string FileName { get; private set; }
+			public LogFileType LogType { get; internal set; }
 
-            public int GetCaseNumberOrId()
+			public int GetCaseNumberOrId()
             {
                 int res;
                 if (int.TryParse(CaseNumber, out res))
@@ -290,8 +294,11 @@ namespace DH.Helpdesk.Services.BusinessLogic.Gdpr
             //delete log files
             foreach (var fileEntity in logFiles)
             {
-                this._filesStorage.DeleteFile(ModuleName.Log, fileEntity.LogId, baseDirPath, fileEntity.FileName);
-            }
+				if (fileEntity.LogType == LogFileType.External)
+					this._filesStorage.DeleteFile(ModuleName.Log, fileEntity.LogId, baseDirPath, fileEntity.FileName);
+				else
+					this._filesStorage.DeleteFile(ModuleName.LogInternal, fileEntity.LogId, baseDirPath, fileEntity.FileName);
+			}
         }
 
         private string GetFilesDirPath(int customerId)
@@ -549,7 +556,7 @@ namespace DH.Helpdesk.Services.BusinessLogic.Gdpr
                 if (logFilesEnitites.Any())
                 {
                     //store to delete from disk
-                    var items = logFilesEnitites.Select(l => new CaseFileEntity(c.Id, c.CaseNumber.ToString(), l.Log_Id, l.FileName)).ToList();
+                    var items = logFilesEnitites.Select(l => new CaseFileEntity(c.Id, c.CaseNumber.ToString(), l.Log_Id, l.FileName, l.LogType)).ToList();
                     caseFilesToDelete.AddRange(items);
 
                     //delete from db
