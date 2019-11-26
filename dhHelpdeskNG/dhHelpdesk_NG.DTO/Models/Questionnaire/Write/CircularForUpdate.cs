056c22385f7c6a76a27024c10e6565ecec3e161d
Johan Weinitz <johan.weinitz@dhsolutions.se>
2019-11-26 16:19:50 +0100

    #75298 forgot one file

--- dhHelpdeskNG/dhHelpdesk_NG.DTO/Models/Questionnaire/Write/CircularForUpdate.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.DTO/Models/Questionnaire/Write/CircularForUpdate.cs
@@ -7,20 +7,16 @@ namespace DH.Helpdesk.BusinessData.Models.Questionnaire.Write
 
     public sealed class CircularForUpdate : Circular
     {
-        public CircularForUpdate(int id, string circularName, DateTime changedDate, List<int> relatedCaseIds, CircularCaseFilter caseFilter, List<string> extraEmails = null)
+        public CircularForUpdate(int id, string circularName, DateTime changedDate, CircularCaseFilter caseFilter, List<string> extraEmails = null)
             : base(circularName)
         {
             this.Id = id;
             this.ChangedDate = changedDate;
-            this.RelatedCaseIds = relatedCaseIds;
             CaseFilter = caseFilter;
             ExtraEmails = extraEmails ?? new List<string>();
         }
 
         public DateTime ChangedDate { get; private set; }
-
-        [NotNull]
-        public List<int> RelatedCaseIds { get; private set; }
 
         public CircularCaseFilter CaseFilter { get; set; }
 
