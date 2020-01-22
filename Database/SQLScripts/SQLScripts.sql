c7b546619347c7b81ff7dfdd5b37a74a66035722
Mikhail Romanyuk <romanyuk_m@outlook.com>
2020-01-22 11:02:09 +0300

    #76935 - Added Status field creating in tblCustomer if it don't exists

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -197,6 +197,13 @@ BEGIN
     ADD UseInitiatorAutocomplete bit not null default 1 
 END
 
+RAISERROR ('Add Status to tblCustomer table', 10, 1) WITH NOWAIT
+if not exists (select * from syscolumns inner join sysobjects on sysobjects.id = syscolumns.id              
+		 where syscolumns.name = N'Status' and sysobjects.name = N'tblCustomer')
+BEGIN
+    ALTER TABLE tblCustomer
+    ADD [Status] Int not null default 1 
+END
 
 
 -- Last Line to update database version
