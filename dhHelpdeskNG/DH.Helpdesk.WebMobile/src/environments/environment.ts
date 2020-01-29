64dc8042e8f66a4c5d0314ce81c59e04966a4474
sg <sg@dhsolutions.se>
2020-01-29 12:47:55 +0100

    #78669 New New CaseBinding: Computer type description

--- dhHelpdeskNG/DH.Helpdesk.WebMobile/src/environments/environment.ts
+++ dhHelpdeskNG/DH.Helpdesk.WebMobile/src/environments/environment.ts
@@ -13,8 +13,8 @@ export const environment = {
 
 export const config = {
     // apiUrl: 'http://dhutvas3.datahalland.se:8049',
-    //apiUrl: 'http://localhost:8049',
-    apiUrl: 'http://192.168.1.102:8049',
+    apiUrl: 'http://localhost:8049',
+    //apiUrl: 'http://192.168.1.102:8049',
     clientId: 'hd',
     version: VERSION.fullVersion,
     internalVersion: require('../../package.json').version,
