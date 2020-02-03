f2b425c96e4ce419daf04ca8a82ae44319a2e654
Mikhail Romanyuk <romanyuk_m@outlook.com>
2020-02-03 13:09:10 +0300

    #78181 - query fix for multiply customers

--- dhHelpdeskNG/dhHelpdesk_NG.Web_SelfService/Controllers/CaseController.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web_SelfService/Controllers/CaseController.cs
@@ -739,13 +739,8 @@ namespace DH.Helpdesk.SelfService.Controllers
                     .ToList()
                 : new List<string>();
 
-            var initiator = string.IsNullOrEmpty(userId) ? null : _computerService.GetComputerUserByUserID(userId, SessionFacade.CurrentCustomer.Id);
-            var showOnExtPageDepartmentCases = initiator?.ShowOnExtPageDepartmentCases ?? false;
-            var initiatorDepartmentId = showOnExtPageDepartmentCases ? (int?) null : initiator?.Department_Id;
-            
             var customers = _customerUserService.ListCustomersByUserCases(userId, employeeNumber, employees,
-                SessionFacade.CurrentCustomer,
-                initiatorDepartmentId);
+                SessionFacade.CurrentCustomer);
 
             var model = new MultiCustomerUserFilterModel
             {
