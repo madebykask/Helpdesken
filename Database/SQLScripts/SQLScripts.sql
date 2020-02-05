60f5a4dc72c47883610234485b030c9ea513bbe6
Johan Weinitz <johan.weinitz@dhsolutions.se>
2020-02-05 16:57:47 +0100

    #77307 Script for dropping UseMobileRouting on customer setting and adding it to global settings

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -20,6 +20,25 @@ SELECT C.Id, 30, 0 FROM tblCustomer C
 LEFT JOIN tblReport_tblCustomer RC ON RC.Customer_Id = C.Id AND RC.Report_Id = 30
 WHERE RC.Report_Id IS NULL
 
+
+RAISERROR ('Add UseMobileRouting to tblGlobalSettings', 10, 1) WITH NOWAIT
+if not exists (select * from syscolumns inner join sysobjects on sysobjects.id = syscolumns.id              
+		 where syscolumns.name = N'UseMobileRouting' and sysobjects.name = N'tblGlobalSettings')
+BEGIN
+    ALTER TABLE tblGlobalSettings
+    ADD UseMobileRouting bit not null default 0 
+END
+
+RAISERROR ('Remove UseMobileRouting on tblSettings', 10, 1) WITH NOWAIT
+if  exists (select * from syscolumns inner join sysobjects on sysobjects.id = syscolumns.id              
+		 where syscolumns.name = N'UseMobileRouting' and sysobjects.name = N'tblSettings')
+BEGIN
+    ALTER TABLE tblSettings
+    DROP UseMobileRouting 
+END
+
+
+
 -- Last Line to update database version
 UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.46'
 GO
