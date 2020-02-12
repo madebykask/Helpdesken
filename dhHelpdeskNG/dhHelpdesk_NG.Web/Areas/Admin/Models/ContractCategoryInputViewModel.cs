5ebcfda00064fa7780c8ced183fa65f5f74d2e94
sg <sg@dhsolutions.se>
2020-02-12 10:56:36 +0100

    #79328 Contract Category - Only first level of Casetype

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Areas/Admin/Models/ContractCategoryInputViewModel.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Areas/Admin/Models/ContractCategoryInputViewModel.cs
@@ -4,13 +4,17 @@
     using System.Web.Mvc;
 
     using DH.Helpdesk.Domain;
+    using BusinessData.Models.Case.Output;
 
     public class ContractCategoryInputViewModel
     {
         public ContractCategory ContractCategory { get; set; }
         public Customer Customer { get; set; }
 
-        public IList<SelectListItem> CaseType { get; set; }
+        public IList<CaseTypeOverview> CaseTypes { get; set; }
+        public int CaseType_Id { get; set; }
+        public string ParentPath_CaseType { get; set; }
+
         public IList<SelectListItem> StateSecondary { get; set; }
     }
 }
