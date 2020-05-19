4e5234c7c485ea35ae53d81bdcc5d6ce7665277a
sg <sg@dhsolutions.se>
2020-05-19 08:42:02 +0200

    Fix Script, PRoblem causing by merg to Dav from Master

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -1,7 +1,12 @@
 ﻿--update DB from 5.3.46 to 5.3.47 version
 
-if not exists (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_tblInventory_tblComputerType]') 
-             AND parent_object_id = OBJECT_ID(N'[dbo].[tblInventory]'))
+RAISERROR ('Add ComputerType_Id FOREIGN KEY to tblInventory', 10, 1) WITH NOWAIT
+if exists (select * from syscolumns inner join sysobjects on sysobjects.id = syscolumns.id              
+		 where syscolumns.name = N'ComputerType_Id' and sysobjects.name = N'tblInventory')
+BEGIN
+    ALTER TABLE tblInventory	
+    WITH CHECK ADD  CONSTRAINT [FK_tblInventory_tblComputerType] FOREIGN KEY([ComputerType_Id])
+REFERENCES [dbo].[tblComputerType] ([Id])
 END
 
 RAISERROR('Insert My department cases feature toggle to tblFeatureToggle', 10, 1) WITH NOWAIT
@@ -9,7 +14,7 @@ IF NOT EXISTS (SELECT * FROM tblFeatureToggle WHERE [StrongName] = 'DISABLE_SELF
 BEGIN
 	INSERT INTO tblFeatureToggle([StrongName], [Active], [Description]) VALUES('DISABLE_SELFSERVICE_SETTING_VIEW_DEPARTMENT_CASES', 
 	0,'Disable the the user interface for administration of initiators access to department cases. Toggle disables the admin interface, not the functionality.')
-
+END
 -- Last Line to update database version
 UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.47'
 GO
