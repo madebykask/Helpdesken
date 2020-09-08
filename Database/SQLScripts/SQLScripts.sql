96d232a86ae69cfa73fd4699448206f09399f0b7
sg <sg@dhsolutions.se>
2020-09-08 16:26:30 +0200

    Start of version 48

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -1,11 +1,6 @@
-﻿--update DB from 5.3.47 to 5.3.48 version
-
-
-RAISERROR('Increase NovelClient column lenght in tblComputer', 10, 1) WITH NOWAIT
-ALTER TABLE dbo.tblComputer
-  ALTER COLUMN NovellClient NVARCHAR(100) NOT NULL;
+﻿--update DB from 5.3.48 to 5.3.49 version
 
 
   -- Last Line to update database version
-UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.48'
+UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.49'
 GO
---
+++ Database/SQLScripts/ScriptVersions/v5.3.48.sql
@@ -0,0 +1,11 @@
+﻿--update DB from 5.3.47 to 5.3.48 version
+
+
+RAISERROR('Increase NovelClient column lenght in tblComputer', 10, 1) WITH NOWAIT
+ALTER TABLE dbo.tblComputer
+  ALTER COLUMN NovellClient NVARCHAR(100) NOT NULL;
+
+
+  -- Last Line to update database version
+UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.48'
+GO
