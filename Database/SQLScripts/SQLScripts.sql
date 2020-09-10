990db56afaab683538b70ecde4faa64525c496ff
sg <sg@dhsolutions.se>
2020-09-10 13:50:13 +0200

    #90165 FIX Script

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -1,6 +1,26 @@
-﻿--update DB from 5.3.48 to 5.3.49 version
+﻿--update DB from 5.3.47 to 5.3.48 version
 
+RAISERROR('Increase NovelClient column lenght in tblComputer', 10, 1) WITH NOWAIT
+ALTER TABLE dbo.tblComputer
+  ALTER COLUMN NovellClient NVARCHAR(100) NOT NULL;
+
+RAISERROR ('Add ServerFileName to tblServer', 10, 1) WITH NOWAIT
+if not exists (select * from syscolumns inner join sysobjects on sysobjects.id = syscolumns.id              
+		 where syscolumns.name = N'ServerFileName' and sysobjects.name = N'tblServer')
+BEGIN
+    ALTER TABLE tblServer
+    ADD ServerFileName nvarchar(100) 
+END
+
+RAISERROR ('Add ServerDocument to tblServer', 10, 1) WITH NOWAIT
+if not exists (select * from syscolumns inner join sysobjects on sysobjects.id = syscolumns.id              
+		 where syscolumns.name = N'ServerDocument' and sysobjects.name = N'tblServer')
+BEGIN
+    ALTER TABLE tblServer
+    ADD ServerDocument image   
+END
 
   -- Last Line to update database version
-UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.49'
+UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.48'
 GO
+
