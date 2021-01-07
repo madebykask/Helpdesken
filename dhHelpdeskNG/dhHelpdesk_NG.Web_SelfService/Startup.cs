3a915f7e87a5c4d76023628d9ee6d8f66bcb4755
sg <soniya.ghorbani@dhsolutions.se>
2021-01-07 14:01:36 +0100

    #100592 Fix authentication without changes in webconfig

--- dhHelpdeskNG/dhHelpdesk_NG.Web_SelfService/Startup.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web_SelfService/Startup.cs
@@ -18,17 +18,23 @@ namespace DH.Helpdesk.SelfService
     public class Startup
     {
         // The Client ID is used by the application to uniquely identify itself to Microsoft identity platform.
-        string clientId = System.Configuration.ConfigurationManager.AppSettings["ClientId"];
+        string clientId = System.Configuration.ConfigurationManager.AppSettings["ClientId"] != null ?
+                          System.Configuration.ConfigurationManager.AppSettings["ClientId"] : "";
 
         // RedirectUri is the URL where the user will be redirected to after they sign in.
-        string redirectUri = System.Configuration.ConfigurationManager.AppSettings["RedirectUri"];
+        string redirectUri = System.Configuration.ConfigurationManager.AppSettings["RedirectUri"] != null ?
+                             System.Configuration.ConfigurationManager.AppSettings["RedirectUri"] : "";
 
         // Tenant is the tenant ID (e.g. contoso.onmicrosoft.com, or 'common' for multi-tenant)
-        static string tenant = System.Configuration.ConfigurationManager.AppSettings["Tenant"];
+        static string tenant = System.Configuration.ConfigurationManager.AppSettings["Tenant"] != null ?
+                               System.Configuration.ConfigurationManager.AppSettings["Tenant"] : "";
 
+        static string auth = System.Configuration.ConfigurationManager.AppSettings["Authority"] != null ?
+                               System.Configuration.ConfigurationManager.AppSettings["Authority"] : "";
         // Authority is the URL for authority, composed by Microsoft identity platform endpoint and the tenant name (e.g. https://login.microsoftonline.com/contoso.onmicrosoft.com/v2.0)
-        string authority = String.Format(System.Globalization.CultureInfo.InvariantCulture, System.Configuration.ConfigurationManager.AppSettings["Authority"], tenant);
+        string authority = String.Format(System.Globalization.CultureInfo.InvariantCulture, auth, tenant);
 
+        string loginMode = System.Configuration.ConfigurationManager.AppSettings["LoginMode"];
 
         public void Configuration(IAppBuilder app)
         {
@@ -42,34 +48,38 @@ namespace DH.Helpdesk.SelfService
                 CookieSecure = CookieSecureOption.Always
             };
             app.UseCookieAuthentication(cookieOptions);
-            app.UseOpenIdConnectAuthentication(
-                new OpenIdConnectAuthenticationOptions
-                {
-                    // Sets the ClientId, authority, RedirectUri as obtained from web.config
-                    ClientId = clientId,
-                    Authority = authority,
-                    RedirectUri = redirectUri,
-                    // PostLogoutRedirectUri is the page that users will be redirected to after sign-out. In this case, it is using the home page
-                    PostLogoutRedirectUri = redirectUri,
-                    Scope = OpenIdConnectScope.OpenIdProfile,
-                    // ResponseType is set to request the id_token - which contains basic information about the signed-in user
-                    ResponseType = OpenIdConnectResponseType.IdToken,
-                    // ValidateIssuer set to false to allow personal and work accounts from any organization to sign in to your application
-                    // To only allow users from a single organizations, set ValidateIssuer to true and 'tenant' setting in web.config to the tenant name
-                    // To allow users from only a list of specific organizations, set ValidateIssuer to true and use ValidIssuers parameter
-                    TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        ValidateIssuer = false // This is a simplification
-                    },
-                    // OpenIdConnectAuthenticationNotifications configures OWIN to send notification of failed authentications to OnAuthenticationFailed method
-                    Notifications = new OpenIdConnectAuthenticationNotifications
-                    {
-                        AuthenticationFailed = OnAuthenticationFailed
-                    },
-                    // Just for debug purpose to see cookies values
-                    //CookieManager = new SameSiteCookieManager(new SystemWebCookieManager())
-                }
-            );
+            if(loginMode == "Microsoft")
+            {
+                app.UseOpenIdConnectAuthentication(
+                                new OpenIdConnectAuthenticationOptions
+                                {
+                                    // Sets the ClientId, authority, RedirectUri as obtained from web.config
+                                    ClientId = clientId,
+                                    Authority = authority,
+                                    RedirectUri = redirectUri,
+                                    // PostLogoutRedirectUri is the page that users will be redirected to after sign-out. In this case, it is using the home page
+                                    PostLogoutRedirectUri = redirectUri,
+                                    Scope = OpenIdConnectScope.OpenIdProfile,
+                                    // ResponseType is set to request the id_token - which contains basic information about the signed-in user
+                                    ResponseType = OpenIdConnectResponseType.IdToken,
+                                    // ValidateIssuer set to false to allow personal and work accounts from any organization to sign in to your application
+                                    // To only allow users from a single organizations, set ValidateIssuer to true and 'tenant' setting in web.config to the tenant name
+                                    // To allow users from only a list of specific organizations, set ValidateIssuer to true and use ValidIssuers parameter
+                                    TokenValidationParameters = new TokenValidationParameters()
+                                    {
+                                        ValidateIssuer = false // This is a simplification
+                                    },
+                                    // OpenIdConnectAuthenticationNotifications configures OWIN to send notification of failed authentications to OnAuthenticationFailed method
+                                    Notifications = new OpenIdConnectAuthenticationNotifications
+                                    {
+                                        AuthenticationFailed = OnAuthenticationFailed
+                                    },
+                                    // Just for debug purpose to see cookies values
+                                    //CookieManager = new SameSiteCookieManager(new SystemWebCookieManager())
+                                }
+                             );
+            }
+            
         }
 
 
