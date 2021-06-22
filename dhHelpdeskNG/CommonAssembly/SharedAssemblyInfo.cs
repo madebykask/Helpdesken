5145e6479602302d8a6e02abeab2601ee132b7f6
Katarina Ask <katarina.ask@dhsolutions.se>
2021-06-22 11:05:33 +0200

    v5.3.52

--- dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
+++ dhHelpdeskNG/CommonAssembly/SharedAssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyCompany("DH Solutions")]
-[assembly: AssemblyCopyright("© 2015 DH Solutions")]
-[assembly: AssemblyVersion("5.3.44.4")]
-[assembly: AssemblyFileVersion("5.3.44.4")]
-[assembly: AssemblyInformationalVersion("5.3.44.4-25ae3b760")]
+[assembly: AssemblyCopyright("© 2021 DH Solutions")]
+[assembly: AssemblyVersion("5.3.52.1")]
+[assembly: AssemblyFileVersion("5.3.52.1")]
+[assembly: AssemblyInformationalVersion("5.3.52.1")]
 
 namespace DH.Helpdesk
 {
     public static class Version
     {
-        public const string FULL_VERSION = "5.3.44.4-25ae3b760";
+        public const string FULL_VERSION = "5.3.52.1";
     }
 }
