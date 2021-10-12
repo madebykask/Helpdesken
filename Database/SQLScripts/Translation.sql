951d074df8df72a6249ddd0cb5022210dd8b47c7
DATAHALLAND\ds <david.sampaio@dhsolutions.se>
2021-10-12 15:19:19 +0200

    Fix. Selecting languages

--- Database/SQLScripts/Translation.sql
+++ Database/SQLScripts/Translation.sql
@@ -8151,6 +8151,13 @@ If not exists (select * from tblTextTranslation where text_id = 2096 and Languag
 	insert into tblTextTranslation(Text_Id, Language_Id, TextTranslation) VALUES(2096, 2, 'All text needs to be entered in language')
 GO
 
+If not exists (select * from tbltext where id = 2097)
+	insert into tbltext (id, TextString) VALUES (2097, 'Fälttext')
+GO
+If not exists (select * from tblTextTranslation where text_id = 2097 and Language_Id = 2)
+	insert into tblTextTranslation(Text_Id, Language_Id, TextTranslation) VALUES(2097, 2, 'Field Text')
+GO
+
 -- Generate id sequence for customer generated IDs
 If not exists (select * from tbltext where id = 20000)
 	insert into tbltext (id, TextString) VALUES (20000, 'Start för kundsekvens av systemgenererade ID:n (ignore)')
