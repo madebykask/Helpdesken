6738cb62405d909c362edf9f1e20b2e9c5137564
Mikhail Ramaniuk <romanyuk_m@outlook.com>
2021-11-08 13:46:48 +0300

    #170350 - changed way how translations apply

--- ExtendedCase/ExtendedCase.Dal/Repositories/TranslationRepository.cs
+++ ExtendedCase/ExtendedCase.Dal/Repositories/TranslationRepository.cs
@@ -25,10 +25,23 @@ namespace ExtendedCase.Dal.Repositories
             var res = QueryList<dynamic>(sql, new { languageId }).ToDictionary(x => (string)x.Property, y => (string)y.Text);
             return res;
         }
+
+        public IDictionary<string, string> GetTranslations(int languageId, IEnumerable<string> values)
+        {
+            var value = string.Join("','", values);
+            var sql = $@"
+                    SELECT Property, Text FROM ExtendedCaseTranslations 
+                    WHERE LanguageId = @languageId AND Property in ('{value}')
+                ";
+
+            var res = QueryList<dynamic>(sql, new { languageId }).ToDictionary(x => (string)x.Property, y => (string)y.Text);
+            return res;
+        }
     }
 
     public interface ITranslationRepository
     {
         IDictionary<string, string> GetTranslations(int languageId);
+        IDictionary<string, string> GetTranslations(int languageId, IEnumerable<string> values);
     }
 }
