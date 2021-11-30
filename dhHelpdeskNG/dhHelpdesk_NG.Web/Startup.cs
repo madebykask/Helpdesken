a4cfaeeaeed95b7b465d5a5ca760c7ccb5c40b10
Katta Ask <katarina.ask@dhsolutions.se>
2021-11-30 17:00:06 +0100

    #159166 - Trying to extent expiration time for ms login

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Startup.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Startup.cs
@@ -46,9 +46,9 @@ namespace DH.Helpdesk.Web.App_Start
                 //SameSiteMode.None should be always with Secure = true in chrome https://docs.microsoft.com/en-us/aspnet/core/security/samesite?view=aspnetcore-5.0
                 CookieSameSite = SameSiteMode.None,
                 CookieSecure = CookieSecureOption.Always,
-                CookieName ="HDCookie"
-                //SlidingExpiration = true,
-                //ExpireTimeSpan = TimeSpan.FromHours(2.0),
+                CookieName ="HDCookie",
+                SlidingExpiration = false,
+                ExpireTimeSpan = TimeSpan.FromHours(2.0),
                 //LoginPath = new PathString("/Login/Login")
             };
             app.UseCookieAuthentication(cookieOptions);
