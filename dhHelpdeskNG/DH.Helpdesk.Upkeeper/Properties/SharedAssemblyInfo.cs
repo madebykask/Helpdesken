83a61095d5d6309d15c3eb5802dfdb7a759023b6
dh_hakan_gunnarsson <hakan.gunnarsson@dhsolutions.se>
2022-01-24 08:27:13 +0100

    Upkeeper assembly-info

---
+++ dhHelpdeskNG/DH.Helpdesk.Upkeeper/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("DH Solutions")]
+[assembly: AssemblyCopyright("© 2021 DH Solutions")]
+[assembly: AssemblyVersion("5.3.54.3")]
+[assembly: AssemblyFileVersion("5.3.54.3")]
+[assembly: AssemblyInformationalVersion("5.3.54.3-c5d9c02c6")]
+
+namespace DH.Helpdesk
+{
+    public static class Version
+    {
+        public const string FULL_VERSION = "5.3.54.3-c5d9c02c6";
+    }
+}
