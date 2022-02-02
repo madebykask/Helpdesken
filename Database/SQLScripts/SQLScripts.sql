c74717017817a61615da35cad72dbfaeadd5db01
Katta Ask <katarina.ask@dhsolutions.se>
2022-02-02 13:58:50 +0100

    #179704 - Task - Case templates - Possibility for different language of General section in Templates

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -14,6 +14,47 @@ if not exists(select * from tblReport WHERE Id=18)
 		INSERT INTO tblReport(Id) VALUES(18)
 	end
 GO
+
+RAISERROR ('Create table tblCaseSolution_tblLanguage', 10, 1) WITH NOWAIT
+IF(OBJECT_ID('tblCaseSolution_tblLanguage', 'U') IS NULL)
+	Begin
+
+		CREATE TABLE [dbo].[tblCaseSolution_tblLanguage](
+			[CaseSolution_Id] [int] NOT NULL,
+			[Language_Id] [int] NOT NULL,
+			[CaseSolutionName] [nvarchar](50) NOT NULL,
+			[ShortDescription] [nvarchar](100) NOT NULL,
+			[Information] [nvarchar](MAX) NULL,
+		 CONSTRAINT [PK_tblCaseSolution_tblLanguage] PRIMARY KEY CLUSTERED 
+		(
+			[CaseSolution_Id] ASC,
+			[Language_Id] ASC
+		)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+		) ON [PRIMARY]
+
+		ALTER TABLE [dbo].[tblCaseSolution_tblLanguage] ADD  CONSTRAINT [DF_tblCaseSolution_tblLanguage_CaseSolutionName]  DEFAULT ('') FOR [CaseSolutionName]
+
+
+		ALTER TABLE [dbo].[tblCaseSolution_tblLanguage] ADD  CONSTRAINT [DF_tblCaseSolution_tblLanguage_ShortDescription]  DEFAULT ('') FOR [ShortDescription]
+
+
+		ALTER TABLE [dbo].[tblCaseSolution_tblLanguage] ADD  CONSTRAINT [DF_tblCaseSolution_tblLanguage_Information]  DEFAULT ('') FOR [Information]
+
+
+		ALTER TABLE [dbo].[tblCaseSolution_tblLanguage]  WITH CHECK ADD  CONSTRAINT [FK_tblCaseSolution_tblLanguage_tblCaseSolution] FOREIGN KEY([CaseSolution_Id])
+		REFERENCES [dbo].[tblCaseSolution] ([Id])
+
+
+		ALTER TABLE [dbo].[tblCaseSolution_tblLanguage] CHECK CONSTRAINT [FK_tblCaseSolution_tblLanguage_tblCaseSolution]
+
+
+		ALTER TABLE [dbo].[tblCaseSolution_tblLanguage] WITH CHECK ADD  CONSTRAINT [FK_tblCaseSolution_tblLanguage_tblLanguage] FOREIGN KEY([Language_Id])
+		REFERENCES [dbo].[tblLanguage] ([Id])
+
+		ALTER TABLE [dbo].[tblCaseSolution_tblLanguage] CHECK CONSTRAINT [FK_tblCaseSolution_tblLanguage_tblLanguage]
+
+	END
+GO
   -- Last Line to update database version
 UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.54'
 GO
---
+++ dhHelpdeskNG/dhHelpdesk_NG.Domain/CaseSolution/CaseSolutionLanguageEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DH.Helpdesk.Domain
+
+{
+    public class CaseSolutionLanguageEntity: EntityBase
+    {
+        public string CaseSolutionName { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string Information { get; set; }
+
+        public int CaseSolution_Id { get; set; }
+
+        public int Language_Id { get; set; }
+
+        public virtual CaseSolution CaseSolution { get; set; }
+    }
+}
