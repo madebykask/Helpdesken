f3e3255b445b3cc64d5f742d46071e0b0f700db4
Katta Ask <katarina.ask@dhsolutions.se>
2022-03-01 14:49:07 +0100

    v5.3.55

--- dhHelpdeskNG/DH.Helpdesk.WebMobile/src/environments/version.ts
+++ dhHelpdeskNG/DH.Helpdesk.WebMobile/src/environments/version.ts
@@ -1,7 +1,7 @@
 ﻿// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT!
 /* tslint:disable */
 export const VERSION = {
-	"version": "5.3.54.3",
-    "fullVersion": "5.3.54.3-c5d9c02c6"
+	"version": "5.3.55.1",
+    "fullVersion": "5.3.55.1-1ba57969"
 }
 /* tslint:enable */
