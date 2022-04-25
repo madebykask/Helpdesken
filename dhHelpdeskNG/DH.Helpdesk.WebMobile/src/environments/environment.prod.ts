15e203deec0dff68bdb25892f3b22d47a8b94a80
Katta Ask <katarina.ask@dhsolutions.se>
2022-04-25 10:58:44 +0200

    Mobile with PS-script

--- dhHelpdeskNG/DH.Helpdesk.WebMobile/src/environments/environment.prod.ts
+++ dhHelpdeskNG/DH.Helpdesk.WebMobile/src/environments/environment.prod.ts
@@ -6,15 +6,15 @@ export const environment = {
 };
 
 export const config = {
-  apiUrl: '${API.URL}',
+  apiUrl: 'API.URL',
   clientId: 'hd',
   version: VERSION.fullVersion,
   internalVersion: require('../../package.json').version,
   enableLog: false,
-  microsoftShowLogin: '${Microsoft.ShowLogin}',
-  microsoftClientId: '${Microsoft.ClientId}',
-  microsoftTenant: '${Microsoft.Tenant}',
-  microsoftAuthority: '${Microsoft.Authority}',
-  microsoftRedirectUri: '${Microsoft.RedirectUri}',
+  microsoftShowLogin: 'Microsoft.ShowLogin',
+  microsoftClientId: 'Microsoft.ClientId',
+  microsoftTenant: 'Microsoft.Tenant',
+  microsoftAuthority: 'Microsoft.Authority',
+  microsoftRedirectUri: 'Microsoft.RedirectUri',
   microsoftDefaultAuthority : "https://login.microsoftonline.com/common/login"
 };
