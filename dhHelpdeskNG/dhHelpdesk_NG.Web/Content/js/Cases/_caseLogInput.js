13378566c988b54bfc157b33dc99a49608aa1357
Katta Ask <katarina.ask@dhsolutions.se>
2022-06-03 08:55:47 +0200

    Revert #213546

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/Cases/_caseLogInput.js
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Content/js/Cases/_caseLogInput.js
@@ -46,17 +46,17 @@ function LogInitForm() {
         informNotifier.removeAttr('checked');
         if (this.value.length) {
             $('#CaseLog_SendMailAboutCaseToNotifier:not(:disabled)').attr('checked', 'checked');
-            var to = $initiatorEmail.val();
-            var extras = $("#extraEmailsInput").val().replaceAll(";", " ");
-            if (to != '') {
-                $("#emailsTo").css('display', 'inline');
-                $("#emailsToTitle").css('display', 'inline');
-            }
-            if (extras != '') {
-                $('#extraEmailsInputShow').text(extras);
-                $('#extraEmailsInputShow').show();
-                $("#ccTitle").show();
-            }
+            //var to = $initiatorEmail.val();
+            //var extras = $("#extraEmailsInput").val().replaceAll(";", " ");
+            //if (to != '') {
+            //    $("#emailsTo").css('display', 'inline');
+            //    $("#emailsToTitle").css('display', 'inline');
+            //}
+            //if (extras != '') {
+            //    $('#extraEmailsInputShow').text(extras);
+            //    $('#extraEmailsInputShow').show();
+            //    $("#ccTitle").show();
+            //}
              
 
         }
