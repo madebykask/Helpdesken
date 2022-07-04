a5ceb3af2fdff3b6e7d554f75e8c7d95d0f4a277
Mattias Arnesson <Mattias.arnesson@dhsolutions.se>
2022-07-04 11:13:18 +0200

    #213298

---
+++ dhHelpdeskNG/DH.Helpdesk.SCCM/Entities/EnclosureWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DH.Helpdesk.SCCM.Entities
+{
+    public class EnclosureWrapper
+    {
+
+        public List<Enclosure> value = new List<Enclosure>();
+
+    }
+}
