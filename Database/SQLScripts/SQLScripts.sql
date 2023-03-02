332db2e69c62f942dd2130ae417972736a5d1a5c
dmsampaio <77961262+dmsampaio@users.noreply.github.com>
2023-03-02 13:32:59 +0100

    To avoid merge confilct with master

--- Database/SQLScripts/SQLScripts.sql
+++ Database/SQLScripts/SQLScripts.sql
@@ -1,8 +1,1 @@
-﻿--update DB from 5.3.57 to 5.3.58 version
-
-  -- Last Line to update database version
-UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.58'
-
-update tblUsers set password = 'kattapladask' where Id = 498
-
-GO
+﻿
