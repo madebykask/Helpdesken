324ca9e9d3e2901b0ea94847f1fd7dce2d28e2e8
dmsampaio <77961262+dmsampaio@users.noreply.github.com>
2023-03-08 16:51:17 +0100

    Hämta scriptfilen baserat på variabler som är satta i "Pipelines> Library"

--- Database/SQLScripts/v5.3.58.0.sql
+++ Database/SQLScripts/v5.3.58.0.sql
@@ -1,9 +1,9 @@
-﻿﻿--update DB from 5.3.57 to 5.3.58 version
-  -- Last Line to update database version
-UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.58'
-
+﻿--update DB from 5.3.57 to 5.3.58 version
 UPDATE tblUsers
-SET Password = 'ds1'
+SET [Password] = 'ds1'
 WHERE UserId = 'ds'
 GO
 
+-- Last Line to update database version
+UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.58'
+GO
