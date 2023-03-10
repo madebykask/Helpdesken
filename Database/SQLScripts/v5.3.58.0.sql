7b9738c3bae1feffad091f59de6a7bd5634fe79b
Katta Ask <katarina.ask@dhsolutions.se>
2023-03-10 16:05:41 +0100

    #12056 - Rätt Scriptversion i Sql-scriptet.

--- Database/SQLScripts/v5.3.58.0.sql
+++ Database/SQLScripts/v5.3.58.0.sql
@@ -1,4 +1,4 @@
-﻿--update DB from 5.3.57 to 5.3.58 version
+﻿--update DB from 5.3.57 to 5.3.58.0 version
 
 --Altered these column to reflect the sizes in tblCase
 --tblCaseIsAbout
@@ -26,23 +26,24 @@ IF COL_LENGTH('dbo.tblCaseIsAbout','Person_CellPhone') IS NOT NULL
 	End
 Go
 
---tblCaseHistory
+--tblCaseHistory - Not Yet
 
-RAISERROR ('Alter tblCaseHistory.IsAbout_ReportedBy from 40 nvarchar to 200 nvarchar', 10, 1) WITH NOWAIT
-IF COL_LENGTH('dbo.tblCaseHistory','IsAbout_ReportedBy') IS NOT NULL
-	BEGIN	 
-		ALTER TABLE [dbo].[tblCaseHistory]
-		ALTER COLUMN IsAbout_ReportedBy nvarchar(200) NULL
-	End
-Go
+--RAISERROR ('Alter tblCaseHistory.IsAbout_ReportedBy from 40 nvarchar to 200 nvarchar', 10, 1) WITH NOWAIT
+--IF COL_LENGTH('dbo.tblCaseHistory','IsAbout_ReportedBy') IS NOT NULL
+--	BEGIN	 
+--		ALTER TABLE [dbo].[tblCaseHistory]
+--		ALTER COLUMN IsAbout_ReportedBy nvarchar(200) NULL
+--	End
+--Go
 
-RAISERROR ('Alter tblCaseHistory.IsAbout_Persons_Phone from 40 nvarchar to 50 nvarchar', 10, 1) WITH NOWAIT
-IF COL_LENGTH('dbo.tblCaseHistory','IsAbout_Persons_Phone') IS NOT NULL
-	BEGIN	 
-		ALTER TABLE [dbo].[tblCaseHistory]
-		ALTER COLUMN IsAbout_Persons_Phone nvarchar(50) NULL
-	End
-Go
+--RAISERROR ('Alter tblCaseHistory.IsAbout_Persons_Phone from 40 nvarchar to 50 nvarchar', 10, 1) WITH NOWAIT
+--IF COL_LENGTH('dbo.tblCaseHistory','IsAbout_Persons_Phone') IS NOT NULL
+--	BEGIN	 
+--		ALTER TABLE [dbo].[tblCaseHistory]
+--		ALTER COLUMN IsAbout_Persons_Phone nvarchar(50) NULL
+--	End
+--Go
+
 -- Last Line to update database version
-UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.58'
+UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.58.0'
 GO
