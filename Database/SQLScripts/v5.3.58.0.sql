34e2966292eba59f29a828a66c31cfc605eeee5b
Katta Ask <katarina.ask@dhsolutions.se>
2023-03-14 15:17:28 +0100

    #11914 - 12136 - Ändra kolumnlängd på olika fält i tblCaseHistory

--- Database/SQLScripts/v5.3.58.0.sql
+++ Database/SQLScripts/v5.3.58.0.sql
@@ -26,23 +26,23 @@ IF COL_LENGTH('dbo.tblCaseIsAbout','Person_CellPhone') IS NOT NULL
 	End
 Go
 
---tblCaseHistory - Not Yet
+--tblCaseHistory 
 
---RAISERROR ('Alter tblCaseHistory.IsAbout_ReportedBy from 40 nvarchar to 200 nvarchar', 10, 1) WITH NOWAIT
---IF COL_LENGTH('dbo.tblCaseHistory','IsAbout_ReportedBy') IS NOT NULL
---	BEGIN	 
---		ALTER TABLE [dbo].[tblCaseHistory]
---		ALTER COLUMN IsAbout_ReportedBy nvarchar(200) NULL
---	End
---Go
+RAISERROR ('Alter tblCaseHistory.IsAbout_ReportedBy from 40 nvarchar to 200 nvarchar', 10, 1) WITH NOWAIT
+IF COL_LENGTH('dbo.tblCaseHistory','IsAbout_ReportedBy') IS NOT NULL
+	BEGIN	 
+		ALTER TABLE [dbo].[tblCaseHistory]
+		ALTER COLUMN IsAbout_ReportedBy nvarchar(200) NULL
+	End
+Go
 
---RAISERROR ('Alter tblCaseHistory.IsAbout_Persons_Phone from 40 nvarchar to 50 nvarchar', 10, 1) WITH NOWAIT
---IF COL_LENGTH('dbo.tblCaseHistory','IsAbout_Persons_Phone') IS NOT NULL
---	BEGIN	 
---		ALTER TABLE [dbo].[tblCaseHistory]
---		ALTER COLUMN IsAbout_Persons_Phone nvarchar(50) NULL
---	End
---Go
+RAISERROR ('Alter tblCaseHistory.IsAbout_Persons_Phone from 40 nvarchar to 50 nvarchar', 10, 1) WITH NOWAIT
+IF COL_LENGTH('dbo.tblCaseHistory','IsAbout_Persons_Phone') IS NOT NULL
+	BEGIN	 
+		ALTER TABLE [dbo].[tblCaseHistory]
+		ALTER COLUMN IsAbout_Persons_Phone nvarchar(50) NULL
+	End
+Go
 
 -- Last Line to update database version
 UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.3.58.0'
