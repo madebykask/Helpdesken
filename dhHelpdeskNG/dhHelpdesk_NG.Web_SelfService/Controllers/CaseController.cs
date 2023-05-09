386b5f89a2e0cbe657cc3103e6b48996ba36cca5
madebykask <75319675+madebykask@users.noreply.github.com>
2023-05-09 14:03:17 +0200

    #13107 - Knappar synliga i Summernote för SS

--- dhHelpdeskNG/dhHelpdesk_NG.Web_SelfService/Controllers/CaseController.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web_SelfService/Controllers/CaseController.cs
@@ -413,11 +413,12 @@ namespace DH.Helpdesk.SelfService.Controllers
                     {
                         LogType = 0,
                         LogGuid = Guid.NewGuid(),
-                        TextExternal = caseTemplate.Text_External,
-                        TextInternal = caseTemplate.Text_Internal,
+                        TextExternal = caseTemplate.Text_External.Replace("\r\n", "<br />"),
+                        TextInternal = caseTemplate.Text_Internal.Replace("\r\n", "<br />"),
                         FinishingType = caseTemplate.FinishingCause_Id
                     };
                 }
+                model.NewCase.Description = caseTemplate.Description.Replace("\r\n", "<br />");
             }
 
             #endregion // Load Case Template
