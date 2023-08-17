9bd02ed21b9006987f3d1b2af437ee2e01784621
Katarina Ask <75319675+madebykask@users.noreply.github.com>
2023-08-17 16:09:55 +0200

    #13563 - 14118 - Utökat kolumner ProductAreas & CaseTypes i GdprFavorite

--- Database/SQLScripts/v5.4.0.0.sql
+++ Database/SQLScripts/v5.4.0.0.sql
@@ -1,13 +1,12 @@
 ﻿--update DB from 5.3.58.2 to 5.4.0.0 version
 
-
--- Last Line to update database version
-UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.4.0.0'
-GO
-
-ALTER TABLE [dbo].[tblStateSecondary]
-ADD AutocloseDays INT DEFAULT 0;
-GO
+RAISERROR ('Add Column AutocloseDays to tblStateSecondary', 10, 1) WITH NOWAIT
+IF COL_LENGTH('dbo.tblStateSecondary','AutocloseDays') IS NULL
+	BEGIN	 
+		ALTER TABLE [dbo].[tblStateSecondary]
+		ADD AutocloseDays INT DEFAULT 0
+	End
+Go
 
 ALTER TRIGGER [dbo].[TR_CreateCaseNumber] ON [dbo].[tblCase] 
 FOR INSERT
@@ -25,3 +24,26 @@ AS
 	UPDATE tblCase Set CaseNumber = @newCaseNumber WHERE Id=@ID AND CaseNumber=0
 
 	COMMIT TRANSACTION
+
+Go
+
+RAISERROR ('Alter tblGDPRDataPrivacyFavorite.ProductAreas from 256 nvarchar to 1000 nvarchar', 10, 1) WITH NOWAIT
+IF COL_LENGTH('dbo.tblGDPRDataPrivacyFavorite','ProductAreas') IS NOT NULL
+	BEGIN	 
+		ALTER TABLE [dbo].[tblGDPRDataPrivacyFavorite]
+		ALTER COLUMN ProductAreas nvarchar(1000) NULL
+	End
+Go
+
+RAISERROR ('Alter tblGDPRDataPrivacyFavorite.CaseTypes from 256 nvarchar to 1000 nvarchar', 10, 1) WITH NOWAIT
+IF COL_LENGTH('dbo.tblGDPRDataPrivacyFavorite','CaseTypes') IS NOT NULL
+	BEGIN	 
+		ALTER TABLE [dbo].[tblGDPRDataPrivacyFavorite]
+		ALTER COLUMN CaseTypes nvarchar(1000) NULL
+	End
+Go
+-- Last Line to update database version
+UPDATE tblGlobalSettings SET HelpdeskDBVersion = '5.4.0.0'
+GO
+
+
