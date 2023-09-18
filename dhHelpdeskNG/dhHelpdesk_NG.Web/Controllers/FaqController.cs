1e77a42d6ea6bd6cf37c856b15c55fbf7f024369
Katarina Ask <75319675+madebykask@users.noreply.github.com>
2023-09-18 15:50:25 +0200

    ##14259 - 14883 - Limit of characters (2000) is not enough

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Controllers/FaqController.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Controllers/FaqController.cs
@@ -267,7 +267,7 @@ namespace DH.Helpdesk.Web.Controllers
         public ViewResult EditFaq(int id, int languageId, bool showDetails = false)
         {
             var faq = _faqService.GetFaqById(id, languageId);
-            faq.Answer = faq.Answer.Replace("\r\n", "<br>");
+            //faq.Answer = faq.Answer.Replace("\r\n", "<br>");
             if (faq == null)
             {
                 throw new HttpException((int)HttpStatusCode.NotFound, null);
