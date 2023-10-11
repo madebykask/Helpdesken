ee34e16e7dd1ec32303733c7925ff5a3b90b7fed
dh_therese_andreasson <therese.andreasson@dhsolutions.se>
2023-10-11 14:57:44 +0200

    Create performance.sql

---
+++ Database/SQLScripts/performance.sql
@@ -0,0 +1,12 @@
+﻿
+
+--tblCase
+--tblCaseHistory
+--tblCaseSolution
+
+IF (SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'tblCase' AND COLUMN_NAME = 'Description' and DATA_TYPE = 'ntext') IS NOT NULL
+	BEGIN	 
+		Print ('HEJ')
+		-- TODO: ÄNDRA KOLUMN TILL NVARCHAR MAX
+	End
+Go
