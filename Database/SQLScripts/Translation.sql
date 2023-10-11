5ffd19189d0f8f4ba3513e218b0dc1604bb38707
Katarina Ask <75319675+madebykask@users.noreply.github.com>
2023-10-11 15:00:19 +0200

    #14205, 15136 - GUI substatus - Closing reason

--- Database/SQLScripts/Translation.sql
+++ Database/SQLScripts/Translation.sql
@@ -8566,6 +8566,13 @@ If not exists (select * from tblTextTranslation where text_id = 2166 and Languag
 	insert into tblTextTranslation(Text_Id, Language_Id, TextTranslation) VALUES(2166, 2, 'Closing days must be greater than Reminder days')
 GO
 
+If not exists (select * from tbltext where id = 2167)
+	insert into tbltext (id, TextString) VALUES (2167, 'Avslutsorsak måste anges på lägsta nivå')
+GO
+If not exists (select * from tblTextTranslation where text_id = 2167 and Language_Id = 2)
+	insert into tblTextTranslation(Text_Id, Language_Id, TextTranslation) VALUES(2167, 2, 'Closing reason must be specified at the lowest level')
+GO
+
 -- Generate id sequence for customer generated IDs
 If not exists (select * from tbltext where id = 20000)
 	insert into tbltext (id, TextString) VALUES (20000, 'Start för kundsekvens av systemgenererade ID:n (ignore)')
