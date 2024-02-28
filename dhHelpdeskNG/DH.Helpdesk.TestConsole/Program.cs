442230071d2904fddf3ca6239485ba56a5963dcc
Katarina Ask <75319675+madebykask@users.noreply.github.com>
2024-02-28 09:17:21 +0100

    TestConsole med Json

--- dhHelpdeskNG/DH.Helpdesk.TestConsole/Program.cs
+++ dhHelpdeskNG/DH.Helpdesk.TestConsole/Program.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
+using DH.Helpdesk.BusinessData.Models.ExtendedCase;
 namespace DH.Helpdesk.TestConsole
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			//var mailer = new Mail();
+			string metaData = "";
+            var hejAnnaKarin = JsonConvert.DeserializeObject<ExtendedCaseFormJsonModel>(metaData);
+            //var mailer = new Mail();
 
-			//mailer.Send("license@standby.eu", "johan.weinitz@dhsolutions.se", "test subject " + DateTime.Now.ToString(), "test body", "", "smtp.datahalland.se", Guid.NewGuid().ToString());
-			//mailer.Send("license@standby.eu", "johan.weinitz@dhsolutions.se", "test subject", "test body", "", "smtp.office365.com;license@standby.eu;Bjare741!;587;true", Guid.NewGuid().ToString());
-		}
+            //mailer.Send("license@standby.eu", "johan.weinitz@dhsolutions.se", "test subject " + DateTime.Now.ToString(), "test body", "", "smtp.datahalland.se", Guid.NewGuid().ToString());
+            //mailer.Send("license@standby.eu", "johan.weinitz@dhsolutions.se", "test subject", "test body", "", "smtp.office365.com;license@standby.eu;Bjare741!;587;true", Guid.NewGuid().ToString());
+        }
 	}
 }
