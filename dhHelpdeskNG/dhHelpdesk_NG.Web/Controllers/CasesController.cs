ad8944a560c247e3bd61e2024ee1c814575fbeab
Mattias Arnesson <Mattias.arnesson@dhsolutions.se>
2024-03-13 13:23:09 +0100

    #16770 - Kontrollera Bulk Edit On Save Case before

--- dhHelpdeskNG/dhHelpdesk_NG.Web/Controllers/CasesController.cs
+++ dhHelpdeskNG/dhHelpdesk_NG.Web/Controllers/CasesController.cs
@@ -1395,6 +1395,18 @@ namespace DH.Helpdesk.Web.Controllers
                     }
                 }
 
+                //Business rules (Before save)
+                //We need to run this method twice, one that compares against current data, and one that compares against incoming data.
+                var ret = _caseService.ExecuteBusinessActionsError(caseToUpdate.Customer_Id, inputData.FinishDate.ToString(), "", inputData.StateSecondary_Id.ToString());
+                var ret2 = _caseService.ExecuteBusinessActionsError(caseToUpdate.Customer_Id, inputData.FinishDate.ToString(), caseToUpdate.Status_Id.ToString(), caseToUpdate.StateSecondary_Id.ToString());
+
+                //This is a fail, we must skip this iteration
+                if (ret == false || ret2 == false)
+                {
+                    string errorMsg = Translation.Get("Ärendet kan inte stängas pågrund av business rules");
+                    throw new Exception(errorMsg);
+                }
+
                 IDictionary<string, string> errors;
                 var mailSenders = new MailSenders();
                 var customer = _customerService.GetCustomer(caseToUpdate.Customer_Id);
