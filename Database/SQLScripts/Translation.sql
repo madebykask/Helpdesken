a07600442147f6c487dbe2c5d2e96e235a76f6d2
Katta <katarina.ask@dhsolutions.se>
2024-12-11 15:58:44 +0100

    #19084 - BulkEdit - Translations

--- Database/SQLScripts/Translation.sql
+++ Database/SQLScripts/Translation.sql
@@ -8692,6 +8692,20 @@ If not exists (select * from tblTextTranslation where text_id = 2183 and Languag
 	insert into tblTextTranslation(Text_Id, Language_Id, TextTranslation) VALUES(2183, 2, 'Include extended case')
 GO
 
+If not exists (select * from tbltext where id = 2184)
+	insert into tbltext (id, TextString) VALUES (2184, 'Skicka inte avslutsmail till anmälare')
+GO
+If not exists (select * from tblTextTranslation where text_id = 2184 and Language_Id = 2)
+	insert into tblTextTranslation(Text_Id, Language_Id, TextTranslation) VALUES(2184, 2, 'Do not send closure email to initiator')
+GO
+
+If not exists (select * from tbltext where id = 2185)
+	insert into tbltext (id, TextString) VALUES (2185, 'Om kryssrutan markeras, skickas inget avslutsmail till anmälaren.')
+GO
+If not exists (select * from tblTextTranslation where text_id = 2185 and Language_Id = 2)
+	insert into tblTextTranslation(Text_Id, Language_Id, TextTranslation) VALUES(2185, 2, 'If the checkbox is selected, no closure email will be sent to the initiator.')
+GO
+
 -- Generate id sequence for customer generated IDs
 If not exists (select * from tbltext where id = 20000)
 	insert into tbltext (id, TextString) VALUES (20000, 'Start för kundsekvens av systemgenererade ID:n (ignore)')
